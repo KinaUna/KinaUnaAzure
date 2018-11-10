3bed412678c1534fd68610b6ce6a9d80f3b11e5d
Per Rosing Mogensen <per.mogensen@live.com>
2018-11-10 16:47:36 +0100

    Fixed notification behavior and added notifications index page to show all notifications.

--- KinaUnaWeb/Controllers/NotificationsController.cs
+++ KinaUnaWeb/Controllers/NotificationsController.cs
@@ -2,13 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KinaUnaWeb.Data;
 using KinaUnaWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KinaUnaWeb.Controllers
 {
     public class NotificationsController : Controller
     {
+        private readonly WebDbContext _context;
+
+        public NotificationsController(WebDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int Id = 0)
+        {
+            string userId = User.FindFirst("sub")?.Value ?? "NoUser";
+            string userEmail = User.FindFirst("email")?.Value ?? "NoUser";
+            string userTimeZone = User.FindFirst("timezone")?.Value ?? "NoUser";
+            List<WebNotification> notificationsList = await _context.WebNotificationsDb.Where(n => n.To == userId).ToListAsync();
+            notificationsList.Reverse();
+
+            if (notificationsList.Any())
+            {
+                notificationsList = notificationsList.OrderBy(n => n.DateTime).ToList();
+                foreach (WebNotification notif in notificationsList)
+                {
+                    notif.DateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow,
+                        TimeZoneInfo.FindSystemTimeZoneById(userTimeZone));
+                    notif.DateTimeString = notif.DateTime.ToString("dd-MMM-yyyy HH:mm");
+                }
+            }
+            if (Id != 0)
+            {
+                WebNotification notification = await _context.WebNotificationsDb.SingleOrDefaultAsync(n => n.Id == Id);
+                if (notification.To == userId)
+                {
+                    notification.DateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow,
+                        TimeZoneInfo.FindSystemTimeZoneById(userTimeZone));
+                    notification.DateTimeString = notification.DateTime.ToString("dd-MMM-yyyy HH:mm");
+                    ViewBag.SelectedNotification = notification;
+                }
+            }
+            
+            return View(notificationsList);
+        }
+
         public IActionResult ShowNotification(WebNotification notification)
         {
             return PartialView(notification);
--- KinaUnaWeb/wwwroot/signalr/KinaUnaWebNotifications.js
+++ KinaUnaWeb/wwwroot/signalr/KinaUnaWebNotifications.js
@@ -1,61 +1,67 @@
 ﻿let connection = null;
 let notificationsCount = 0;
-let notifationsCounter = document.getElementById("notificationsCounter");
-let notificationsIcon = document.getElementById("notificationBellIcon");
+let notificationsMenuDiv = document.getElementById('notificationsMenu');
+let notifationsCounter = document.getElementById('notificationsCounter');
+let notificationsIcon = document.getElementById('notificationBellIcon');
 let menuToggler = document.getElementById('navbarTogglerButton');
 let togglerCounter = document.getElementById('togglerNotificationsCounter');
 
 function notificationItemClick(event, btn) {
-    event.preventDefault();
-    let isNotificationRead = btn.getAttribute('data-isread');
-    if (isNotificationRead === 'false') {
-        notificationsCount--;
-        if (notificationsCount < 0) {
-            notificationsCount = 0;
-        }
-    }
-    notifationsCounter.innerHTML = notificationsCount;
+    let notificationId = btn.getAttribute('data-notificationid');
+    window.location.href = '/Notifications?Id=' + notificationId;
+    
 }
 
 function markRead(event, btn) {
-    event.preventDefault();
-    var parentBtn = btn.closest('button');
-    var notifId = btn.getAttribute('data-notificationid');
+    let notifId = btn.getAttribute('data-notificationid');
+    let itemsToUpdate = document.getElementsByClassName('notifId' + notifId);
     if (btn.classList.contains('notificationUnread')) {
         notificationsCount--;
         if (notificationsCount < 0) {
             notificationsCount = 0;
         }
-        btn.classList.remove('notificationUnread');
-        btn.classList.add('notificationRead');
-        btn.innerHTML = '<i class="material-icons">drafts</i> Mark as unread</a>';
-        parentBtn.classList.remove('notificationUnread');
-        parentBtn.classList.add('notificationRead');
-        parentBtn.classList.add('bg-dark');
-        connection.invoke("SetRead", notifId).catch(err => console.error(err.toString()));
+        for (let i = itemsToUpdate.length - 1; i >= 0; --i) {
+            itemsToUpdate[i].classList.remove('notificationUnread');
+            itemsToUpdate[i].classList.add('notificationRead');
+            itemsToUpdate[i].innerHTML = '<i class="material-icons">markunread</i> Mark as unread</a>';
+            let parentBtn = itemsToUpdate[i].closest('button');
+            parentBtn.classList.remove('notificationUnread');
+            parentBtn.classList.add('notificationRead');
+            parentBtn.classList.add('bg-dark');
+        }
+        connection.invoke('SetRead', notifId).catch(err => console.error(err.toString()));
     } else {
         notificationsCount++;
         if (notificationsCount < 0) {
             notificationsCount = 0;
         }
-        btn.classList.remove('notificationRead');
-        btn.classList.add('notificationUnread');
-        btn.innerHTML = '<i class="material-icons">markunread</i> Mark as read</a>';
-        parentBtn.classList.remove('notificationRead');
-        parentBtn.classList.remove('bg-dark');
-        parentBtn.classList.add('notificationUnread');
-        connection.invoke("SetUnread", notifId).catch(err => console.error(err.toString()));
+        for (let j = itemsToUpdate.length - 1; j >= 0; --j) {
+            itemsToUpdate[j].classList.remove('notificationRead');
+            itemsToUpdate[j].classList.add('notificationUnread');
+            itemsToUpdate[j].innerHTML = '<i class="material-icons">drafts</i> Mark as read</a>';
+            let parentBtn2 = itemsToUpdate[j].closest('button');
+            parentBtn2.classList.remove('notificationRead');
+            parentBtn2.classList.remove('bg-dark');
+            parentBtn2.classList.add('notificationUnread');
+        }
+        connection.invoke('SetUnread', notifId).catch(err => console.error(err.toString()));
     }
     notifationsCounter.innerHTML = notificationsCount;
     if (notificationsCount === 0) {
         togglerCounter.style.display = "none";
+        notifationsCounter.classList.remove('badge-danger');
+        notifationsCounter.classList.add('badge-secondary');
+    } else {
+        notificationsIcon.classList.add('notificationIconAnimation');
+        notifationsCounter.classList.remove('badge-secondary');
+        notifationsCounter.classList.add('badge-danger');
     }
+    event.stopImmediatePropagation();
 }
 
 function removeNotification(event, btn) {
-    event.preventDefault();
-    var notifId = btn.getAttribute('data-notificationid');
-    var parentBtn = btn.closest('button');
+    let notifId = btn.getAttribute('data-notificationid');
+    let parentBtn = btn.closest('button');
     connection.invoke("DeleteNotification", notifId).catch(err => console.error(err.toString()));
     if (parentBtn.classList.contains('notificationUnread')) {
         notificationsCount--;
@@ -65,29 +71,40 @@ function removeNotification(event, btn) {
     }
     if (notificationsCount === 0) {
         togglerCounter.style.display = "none";
+        notifationsCounter.classList.remove('badge-danger');
+        notifationsCounter.classList.add('badge-secondary');
+    } else {
+        notificationsIcon.classList.add('notificationIconAnimation');
+        notifationsCounter.classList.remove('badge-secondary');
+        notifationsCounter.classList.add('badge-danger');
     }
-    parentBtn.parentNode.removeChild(parentBtn);
-    notifationsCounter.innerHTML = notificationsCount;
 
+    let itemsToRemove = document.getElementsByClassName('notifId' + notifId);
+    for (var i = itemsToRemove.length - 1; i >= 0; --i) {
+        itemsToRemove[i].parentNode.removeChild(itemsToRemove[i]);
+    }
+    notifationsCounter.innerHTML = notificationsCount;
+    event.stopPropagation();
+    return false;
 }
 
 connection = new signalR.HubConnectionBuilder()
-    .withUrl("/webnotificationhub")
+    .withUrl('/webnotificationhub')
     .withHubProtocol(new signalR.protocols.msgpack.MessagePackHubProtocol())
     .configureLogging(signalR.LogLevel.Information)
     .build();
 
-connection.on("ReceiveMessage",
+connection.on('ReceiveMessage',
     function(message) {
-        console.log("Notification: " + message);
+        console.log('Notification: ' + message);
         let parsedMessage = JSON.parse(message);
-        let notificationsMenuDiv = document.getElementById("notificationsMenu");
+        let notificationsMenuDiv = document.getElementById('notificationsMenu');
         
         $.ajax({
-            type: "GET",
-            url: "/Notifications/ShowNotification",
+            type: 'GET',
+            url: '/Notifications/ShowNotification',
             data: parsedMessage,
-            datatype: "html",
+            datatype: 'html',
             async: true,
             success: function (data) {
                 notificationsMenuDiv.innerHTML += data;
@@ -96,34 +113,38 @@ connection.on("ReceiveMessage",
                 console.log(textStatus, errorThrown);
             }
         });
-
-        notificationsIcon.classList.add('notificationIconAnimation');
         if (!parsedMessage.IsRead) {
             notificationsCount++;
         }
         notifationsCounter.innerHTML = notificationsCount;
         togglerCounter.innerHTML = notificationsCount;
-        togglerCounter.style.display = "block";
+        togglerCounter.style.display = 'block';
         if (notificationsCount === 0) {
-            togglerCounter.style.display = "none";
+            togglerCounter.style.display = 'none';
+            notifationsCounter.classList.remove('badge-danger');
+            notifationsCounter.classList.add('badge-secondary');
+        } else {
+            notificationsIcon.classList.add('notificationIconAnimation');
+            notifationsCounter.classList.remove('badge-secondary');
+            notifationsCounter.classList.add('badge-danger');
         }
     }
 );
 let getNotifications = function () {
     notificationsCount = 0;
     notificationsMenuDiv.innerHTML = '';
-    connection.invoke("GetUpdateForUser").catch(err => console.error(err.toString()));
+    connection.invoke('GetUpdateForUser').catch(err => console.error(err.toString()));
 };
 
 connection.start().catch(err => console.error(err.toString()));
 
 $(document).ready(function () {
     $("#notificationsButton").click(function () {
-        let notificationsIcon = document.getElementById("notificationBellIcon");
-        notificationsIcon.classList.remove("notificationIconAnimation");
-        menuToggler.classList.remove("notificationIconAnimation");
-        togglerCounter.style.display = "none";
+        let notificationsIcon = document.getElementById('notificationBellIcon');
+        notificationsIcon.classList.remove('notificationIconAnimation');
+        menuToggler.classList.remove('notificationIconAnimation');
+        togglerCounter.style.display = 'none';
     });
 
-    var checkNotifications = setInterval(getNotifications(), 30000);
+    let checkNotifications = setInterval(getNotifications(), 300000);
 });
