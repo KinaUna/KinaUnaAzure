0052f6dae15a39a29c179647188ce3cafa3870a1
Per Rosing Mogensen <per.mogensen@kinauna.com>
2021-02-24 12:01:28 +0100

    Added PivoqAccess model and database context.

---
+++ KinaUna.Data/Models/PivoqAccess.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KinaUna.Data.Models
+{
+    public class PivoqAccess
+    {
+        [Key]
+        public int AccessId { get; set; }
+        public int ProgenyId { get; set; }
+        public string UserId { get; set; }
+        public int AccessLevel { get; set; } // 0 = Hidden/Parents only, 1=Family, 2= Friends, 3=DefaultUSers, 4= public.
+        public bool CanContribute { get; set; }
+        [NotMapped]
+        public Progeny Progeny { get; set; }
+        [NotMapped]
+        public ApplicationUser User { get; set; }
+        [NotMapped]
+        public string AccessLevelString { get; set; }
+    }
+}
