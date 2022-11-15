7df296ba064ac02c49e49728f05a706d3d86faaa
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2022-11-15 16:45:13 +0100

    Added test for UserInfoService.AddUserInfo().

--- KinaUnaProgenyApi.Tests/Services/UserInfoServiceTests.cs
+++ KinaUnaProgenyApi.Tests/Services/UserInfoServiceTests.cs
@@ -30,7 +30,7 @@ namespace KinaUnaProgenyApi.Tests.Services
                 ViewChild = 1, ProfilePicture = Constants.ProfilePictureUrl, Timezone = Constants.DefaultTimezone
             };
 
-            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("GetProgeny_Should_Return_Progeny_Object_When_Id_Is_Valid").Options;
+            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("GetAllUserInfos_Should_Return_List_Of_UserInfo").Options;
             await using ProgenyDbContext context = new ProgenyDbContext(dbOptions);
             context.Add(userInfo1);
             context.Add(userInfo2);
@@ -56,7 +56,7 @@ namespace KinaUnaProgenyApi.Tests.Services
                 UserEmail = "test1@test.com", UserId = "UserId1", UserName = "Test1", FirstName = "FirstName1", MiddleName = "MiddleName1", LastName = "LastName1",
                 ViewChild = 1, ProfilePicture = Constants.ProfilePictureUrl, Timezone = Constants.DefaultTimezone
             };
-            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("GetProgeny_Should_Return_Progeny_Object_When_Id_Is_Valid").Options;
+            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("GetUserInfoByEmail_Should_Return_UserInfo_Object_When_Email_Is_Valid").Options;
             await using ProgenyDbContext context = new ProgenyDbContext(dbOptions);
             context.Add(userInfo1);
             await context.SaveChangesAsync();
@@ -96,7 +96,7 @@ namespace KinaUnaProgenyApi.Tests.Services
                 ProfilePicture = Constants.ProfilePictureUrl,
                 Timezone = Constants.DefaultTimezone
             };
-            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("GetProgeny_Should_Return_Progeny_Object_When_Id_Is_Valid").Options;
+            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("GetUserInfoByEmail_Should_Return_Null_When_Email_Is_Invalid").Options;
             await using ProgenyDbContext context = new ProgenyDbContext(dbOptions);
             context.Add(userInfo1);
             await context.SaveChangesAsync();
@@ -107,6 +107,60 @@ namespace KinaUnaProgenyApi.Tests.Services
             UserInfo resultUserInfo = await userInfoService.GetUserInfoByEmail("abc@abc.com");
 
             Assert.Null(resultUserInfo);
+        }
+
+        [Fact]
+        public async Task AddUserInfo_Should_Save_UserInfo()
+        {
+            
+            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("AddUserInfo_Should_Save_UserInfo").Options;
+            await using ProgenyDbContext context = new ProgenyDbContext(dbOptions);
+            IOptions<MemoryDistributedCacheOptions>? memoryCacheOptions = Options.Create(new MemoryDistributedCacheOptions());
+            IDistributedCache memoryCache = new MemoryDistributedCache(memoryCacheOptions);
+            UserInfoService userInfoService = new UserInfoService(context, memoryCache);
+
+            UserInfo userInfoToAdd = new UserInfo
+            {
+                UserEmail = "test1@test.com",
+                UserId = "UserId1",
+                UserName = "Test1",
+                FirstName = "FirstName1",
+                MiddleName = "MiddleName1",
+                LastName = "LastName1",
+                ViewChild = 1,
+                ProfilePicture = Constants.ProfilePictureUrl,
+                Timezone = Constants.DefaultTimezone
+            };
+
+            UserInfo addedUserInfo = await userInfoService.AddUserInfo(userInfoToAdd);
+            UserInfo? dbUserInfo = await context.UserInfoDb.AsNoTracking().SingleOrDefaultAsync(ui => ui.Id == addedUserInfo.Id);
+            UserInfo savedUserInfo = await userInfoService.GetUserInfoByEmail(userInfoToAdd.UserEmail);
+
+            Assert.NotNull(savedUserInfo);
+            Assert.IsType<UserInfo>(savedUserInfo);
+            Assert.NotEqual(0, addedUserInfo.Id);
+            Assert.Equal(savedUserInfo.UserEmail, userInfoToAdd.UserEmail);
+            Assert.Equal(savedUserInfo.UserId, userInfoToAdd.UserId);
+            Assert.Equal(savedUserInfo.UserName, userInfoToAdd.UserName);
+            Assert.Equal(savedUserInfo.FirstName, userInfoToAdd.FirstName);
+            Assert.Equal(savedUserInfo.MiddleName, userInfoToAdd.MiddleName);
+            Assert.Equal(savedUserInfo.LastName, userInfoToAdd.LastName);
+            Assert.Equal(savedUserInfo.ViewChild, userInfoToAdd.ViewChild);
+            Assert.Equal(savedUserInfo.ProfilePicture, userInfoToAdd.ProfilePicture);
+            Assert.Equal(savedUserInfo.Timezone, userInfoToAdd.Timezone);
+
+            if (dbUserInfo != null)
+            {
+                Assert.Equal(dbUserInfo.UserEmail, userInfoToAdd.UserEmail);
+                Assert.Equal(dbUserInfo.UserId, userInfoToAdd.UserId);
+                Assert.Equal(dbUserInfo.UserName, userInfoToAdd.UserName);
+                Assert.Equal(dbUserInfo.FirstName, userInfoToAdd.FirstName);
+                Assert.Equal(dbUserInfo.MiddleName, userInfoToAdd.MiddleName);
+                Assert.Equal(dbUserInfo.LastName, userInfoToAdd.LastName);
+                Assert.Equal(dbUserInfo.ViewChild, userInfoToAdd.ViewChild);
+                Assert.Equal(dbUserInfo.ProfilePicture, userInfoToAdd.ProfilePicture);
+                Assert.Equal(dbUserInfo.Timezone, userInfoToAdd.Timezone);
+            }
         }
     }
 }
