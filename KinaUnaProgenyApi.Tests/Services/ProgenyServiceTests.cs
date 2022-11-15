0e3c8d7d453e9c19aeb3cd390e928925df9fb95a
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2022-11-15 14:33:21 +0100

    Added Test for UpdateProgeny.

--- KinaUnaProgenyApi.Tests/Services/ProgenyServiceTests.cs
+++ KinaUnaProgenyApi.Tests/Services/ProgenyServiceTests.cs
@@ -27,6 +27,8 @@ namespace KinaUnaProgenyApi.Tests.Services
             ProgenyService progenyService = new ProgenyService(context, memoryCache);
 
             Progeny resultProgeny = await progenyService.GetProgeny(1);
+            // 2nd call to GetProgeny is retrieving the Progeny object from cache.
+            Progeny resultProgenyCached = await progenyService.GetProgeny(1);
 
             Assert.NotNull(resultProgeny);
             Assert.IsType<Progeny>(resultProgeny);
@@ -36,6 +38,15 @@ namespace KinaUnaProgenyApi.Tests.Services
             Assert.Equal(resultProgeny.NickName, progenyToAdd.NickName);
             Assert.Equal(resultProgeny.PictureLink, progenyToAdd.PictureLink);
             Assert.Equal(resultProgeny.TimeZone, progenyToAdd.TimeZone);
+
+            Assert.NotNull(resultProgenyCached);
+            Assert.IsType<Progeny>(resultProgenyCached);
+            Assert.Equal(resultProgenyCached.BirthDay, progenyToAdd.BirthDay);
+            Assert.Equal(resultProgenyCached.Admins, progenyToAdd.Admins);
+            Assert.Equal(resultProgenyCached.Name, progenyToAdd.Name);
+            Assert.Equal(resultProgenyCached.NickName, progenyToAdd.NickName);
+            Assert.Equal(resultProgenyCached.PictureLink, progenyToAdd.PictureLink);
+            Assert.Equal(resultProgenyCached.TimeZone, progenyToAdd.TimeZone);
         }
 
         [Fact]
@@ -68,16 +79,80 @@ namespace KinaUnaProgenyApi.Tests.Services
                 { BirthDay = DateTime.Now, Admins = "test@test.com", Name = "Test Child A", NickName = "A", PictureLink = Constants.ProfilePictureUrl, TimeZone = Constants.DefaultTimezone };
 
             Progeny addedProgeny = await progenyService.AddProgeny(progenyToAdd);
+            Progeny? dbProgeny = await context.ProgenyDb.SingleOrDefaultAsync(p => p.Id == addedProgeny.Id);
             Progeny savedProgeny = await progenyService.GetProgeny(addedProgeny.Id);
+
+            Assert.NotNull(addedProgeny);
+            Assert.IsType<Progeny>(addedProgeny);
+            Assert.NotEqual(0, addedProgeny.Id);
 
             Assert.NotNull(savedProgeny);
             Assert.IsType<Progeny>(savedProgeny);
+            Assert.NotEqual(0, savedProgeny.Id);
             Assert.Equal(savedProgeny.BirthDay, progenyToAdd.BirthDay);
             Assert.Equal(savedProgeny.Admins, progenyToAdd.Admins);
             Assert.Equal(savedProgeny.Name, progenyToAdd.Name);
             Assert.Equal(savedProgeny.NickName, progenyToAdd.NickName);
             Assert.Equal(savedProgeny.PictureLink, progenyToAdd.PictureLink);
             Assert.Equal(savedProgeny.TimeZone, progenyToAdd.TimeZone);
+
+            if (dbProgeny != null)
+            {
+                Assert.NotNull(dbProgeny);
+                Assert.IsType<Progeny>(dbProgeny);
+                Assert.NotEqual(0, dbProgeny.Id);
+                Assert.Equal(dbProgeny.BirthDay, progenyToAdd.BirthDay);
+                Assert.Equal(dbProgeny.Admins, progenyToAdd.Admins);
+                Assert.Equal(dbProgeny.Name, progenyToAdd.Name);
+                Assert.Equal(dbProgeny.NickName, progenyToAdd.NickName);
+                Assert.Equal(dbProgeny.PictureLink, progenyToAdd.PictureLink);
+                Assert.Equal(dbProgeny.TimeZone, progenyToAdd.TimeZone);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateProgenyShouldSaveProgeny()
+        {
+            DbContextOptions<ProgenyDbContext> dbOptions = new DbContextOptionsBuilder<ProgenyDbContext>().UseInMemoryDatabase("AddProgenyShouldSaveProgeny").Options;
+            using ProgenyDbContext context = new ProgenyDbContext(dbOptions);
+            Progeny originalProgeny = new Progeny
+                { BirthDay = DateTime.Now, Admins = "test@test.com", Name = "Test Child A", NickName = "A", PictureLink = Constants.ProfilePictureUrl, TimeZone = Constants.DefaultTimezone };
+            context.Add(originalProgeny);
+            await context.SaveChangesAsync();
+
+            IOptions<MemoryDistributedCacheOptions>? memoryCacheOptions = Options.Create<MemoryDistributedCacheOptions>(new MemoryDistributedCacheOptions());
+            IDistributedCache memoryCache = new MemoryDistributedCache(memoryCacheOptions);
+            ProgenyService progenyService = new ProgenyService(context, memoryCache);
+
+            Progeny progenyToUpdate = await progenyService.GetProgeny(1);
+            progenyToUpdate.Name = "B";
+
+            Progeny resultProgeny = await progenyService.UpdateProgeny(progenyToUpdate);
+            Progeny? dbProgeny = await context.ProgenyDb.SingleOrDefaultAsync(p => p.Id == resultProgeny.Id);
+            Progeny savedProgeny = await progenyService.GetProgeny(resultProgeny.Id);
+
+            Assert.NotNull(savedProgeny);
+            Assert.IsType<Progeny>(savedProgeny);
+            Assert.Equal(savedProgeny.Id, progenyToUpdate.Id);
+            Assert.Equal(savedProgeny.BirthDay, progenyToUpdate.BirthDay);
+            Assert.Equal(savedProgeny.Admins, progenyToUpdate.Admins);
+            Assert.Equal(savedProgeny.Name, progenyToUpdate.Name);
+            Assert.Equal(savedProgeny.NickName, progenyToUpdate.NickName);
+            Assert.Equal(savedProgeny.PictureLink, progenyToUpdate.PictureLink);
+            Assert.Equal(savedProgeny.TimeZone, progenyToUpdate.TimeZone);
+
+            if (dbProgeny != null)
+            {
+                Assert.NotNull(dbProgeny);
+                Assert.IsType<Progeny>(dbProgeny);
+                Assert.Equal(dbProgeny.Id, progenyToUpdate.Id);
+                Assert.Equal(dbProgeny.BirthDay, progenyToUpdate.BirthDay);
+                Assert.Equal(dbProgeny.Admins, progenyToUpdate.Admins);
+                Assert.Equal(dbProgeny.Name, progenyToUpdate.Name);
+                Assert.Equal(dbProgeny.NickName, progenyToUpdate.NickName);
+                Assert.Equal(dbProgeny.PictureLink, progenyToUpdate.PictureLink);
+                Assert.Equal(dbProgeny.TimeZone, progenyToUpdate.TimeZone);
+            }
         }
     }
 }
