4881fdbae6fc89b628670c34fb0a8b3e001ff451
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2022-11-18 15:06:36 +0100

    Refactored LocationService: Moved caching operations to their own methods.

--- KinaUnaProgenyApi/Services/ILocationService.cs
+++ KinaUnaProgenyApi/Services/ILocationService.cs
@@ -8,16 +8,13 @@ namespace KinaUnaProgenyApi.Services
     {
         Task<Location> GetLocation(int id);
         Task<Location> AddLocation(Location location);
-        Task<Location> SetLocation(int id);
         Task<Location> UpdateLocation(Location location);
         Task<Location> DeleteLocation(Location location);
-        Task RemoveLocation(int id, int progenyId);
         Task<List<Location>> GetLocationsList(int progenyId);
 
         Task<Address> GetAddressItem(int id);
         Task<Address> AddAddressItem(Address addressItem);
         Task<Address> UpdateAddressItem(Address addressItem);
-        Task<Address> SetAddressItem(int id);
         Task RemoveAddressItem(int id);
     }
 }
--- KinaUnaProgenyApi/Services/LocationService.cs
+++ KinaUnaProgenyApi/Services/LocationService.cs
@@ -27,16 +27,35 @@ namespace KinaUnaProgenyApi.Services
 
         public async Task<Location> GetLocation(int id)
         {
-            Location location;
+            Location location = await GetLocationFromCache(id);
+            if (location == null || location.LocationId == 0)
+            {
+                location = await SetLocationInCache(id);
+            }
+
+            return location;
+        }
+
+        private async Task<Location> GetLocationFromCache(int id)
+        {
+            Location location = new Location();
             string cachedLocation = await _cache.GetStringAsync(Constants.AppName + Constants.ApiVersion + "location" + id);
             if (!string.IsNullOrEmpty(cachedLocation))
             {
                 location = JsonConvert.DeserializeObject<Location>(cachedLocation);
             }
-            else
+
+            return location;
+        }
+
+        private async Task<Location> SetLocationInCache(int id)
+        {
+            Location location = await _context.LocationsDb.AsNoTracking().SingleOrDefaultAsync(l => l.LocationId == id);
+            if (location != null)
             {
-                location = await _context.LocationsDb.AsNoTracking().SingleOrDefaultAsync(l => l.LocationId == id);
                 await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "location" + id, JsonConvert.SerializeObject(location), _cacheOptionsSliding);
+
+                _ = await SetLocationsListInCache(location.ProgenyId);
             }
 
             return location;
@@ -46,85 +65,116 @@ namespace KinaUnaProgenyApi.Services
         {
             _ = _context.LocationsDb.Add(location);
             _ = await _context.SaveChangesAsync();
-            _ = await SetLocation(location.LocationId);
+            _ = await SetLocationInCache(location.LocationId);
             return location;
         }
-
-        public async Task<Location> SetLocation(int id)
-        {
-            Location location = await _context.LocationsDb.AsNoTracking().SingleOrDefaultAsync(l => l.LocationId == id);
-            if (location != null)
-            {
-                await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "location" + id, JsonConvert.SerializeObject(location), _cacheOptionsSliding);
-
-                List<Location> locationsList = await _context.LocationsDb.AsNoTracking().Where(l => l.ProgenyId == location.ProgenyId).ToListAsync();
-                await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "locationslist" + location.ProgenyId, JsonConvert.SerializeObject(locationsList), _cacheOptionsSliding);
-            }
-
-            return location;
-        }
-
+        
         public async Task<Location> UpdateLocation(Location location)
         {
-            _ = _context.LocationsDb.Update(location);
-            _ = await _context.SaveChangesAsync();
-            _ = await SetLocation(location.LocationId);
+            Location locationToUpdate = await _context.LocationsDb.SingleOrDefaultAsync(l => l.LocationId == location.LocationId);
+            if (locationToUpdate != null)
+            {
+                locationToUpdate.AccessLevel = location.AccessLevel;
+                locationToUpdate.Author = location.Author;
+                locationToUpdate.City = location.City;
+                locationToUpdate.Country = location.Country;
+                locationToUpdate.State = location.State;
+                locationToUpdate.PostalCode = location.PostalCode;
+                locationToUpdate.County = location.County;
+                locationToUpdate.Date = location.Date;
+                locationToUpdate.Latitude = location.Latitude;
+                locationToUpdate.Longitude = location.Longitude;
+                locationToUpdate.DateAdded = location.DateAdded;
+                locationToUpdate.District = location.District;
+                locationToUpdate.HouseNumber = location.HouseNumber;
+                locationToUpdate.LocationNumber = location.LocationNumber;
+                locationToUpdate.Name = location.Name;
+                locationToUpdate.Notes = location.Notes;
+                locationToUpdate.StreetName = location.StreetName;
+                locationToUpdate.ProgenyId = location.ProgenyId;
+                locationToUpdate.Tags = location.Tags;
+                locationToUpdate.Progeny = location.Progeny;
+
+                _ = _context.LocationsDb.Update(locationToUpdate);
+                _ = await _context.SaveChangesAsync();
+                _ = await SetLocationInCache(location.LocationId);
+            }
+            
             return location;
         }
 
         public async Task<Location> DeleteLocation(Location location)
         {
-            await RemoveLocation(location.LocationId, location.ProgenyId);
+            Location locationToDelete = await _context.LocationsDb.SingleOrDefaultAsync(l => l.LocationId == location.LocationId);
+            if (locationToDelete != null)
+            {
+                _context.LocationsDb.Remove(location);
+                await _context.SaveChangesAsync();
+                await RemoveLocationFromCache(location.LocationId, location.ProgenyId);
+            }
 
-            _context.LocationsDb.Remove(location);
-            await _context.SaveChangesAsync();
-            
             return location;
         }
-        public async Task RemoveLocation(int id, int progenyId)
+        public async Task RemoveLocationFromCache(int id, int progenyId)
         {
             await _cache.RemoveAsync(Constants.AppName + Constants.ApiVersion + "location" + id);
 
-            List<Location> locationsList = await _context.LocationsDb.AsNoTracking().Where(l => l.ProgenyId == progenyId).ToListAsync();
-            await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "locationslist" + progenyId, JsonConvert.SerializeObject(locationsList), _cacheOptionsSliding);
+            _ = await SetLocationsListInCache(progenyId);
         }
 
         public async Task<List<Location>> GetLocationsList(int progenyId)
         {
-            List<Location> locationsList;
+            List<Location> locationsList = await GetLocationsListFromCache(progenyId);
+            if (!locationsList.Any())
+            {
+                locationsList = await SetLocationsListInCache(progenyId);
+            }
+            return locationsList;
+        }
+
+        private async Task<List<Location>> GetLocationsListFromCache(int progenyId)
+        {
+            List<Location> locationsList = new List<Location>();
             string cachedLocationsList = await _cache.GetStringAsync(Constants.AppName + Constants.ApiVersion + "locationslist" + progenyId);
             if (!string.IsNullOrEmpty(cachedLocationsList))
             {
                 locationsList = JsonConvert.DeserializeObject<List<Location>>(cachedLocationsList);
             }
-            else
-            {
-                locationsList = await _context.LocationsDb.AsNoTracking().Where(l => l.ProgenyId == progenyId).ToListAsync();
-                await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "locationslist" + progenyId, JsonConvert.SerializeObject(locationsList), _cacheOptionsSliding);
-            }
 
             return locationsList;
         }
 
+        private async Task<List<Location>> SetLocationsListInCache(int progenyId)
+        {
+            List<Location> locationsList = await _context.LocationsDb.AsNoTracking().Where(l => l.ProgenyId == progenyId).ToListAsync();
+            await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "locationslist" + progenyId, JsonConvert.SerializeObject(locationsList), _cacheOptionsSliding);
+
+            return locationsList;
+        }
         public async Task<Address> GetAddressItem(int id)
         {
-            Address address;
-            string cachedAddress = await _cache.GetStringAsync(Constants.AppName + Constants.ApiVersion + "address" + id);
-            if (!string.IsNullOrEmpty(cachedAddress))
+            Address address = await GetAddressFromCache(id);
+            if (address == null || address.AddressId == 0)
             {
-                address = JsonConvert.DeserializeObject<Address>(cachedAddress);
-            }
-            else
-            {
-                address = await _context.AddressDb.AsNoTracking().SingleOrDefaultAsync(a => a.AddressId == id);
-                await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "address" + id, JsonConvert.SerializeObject(address), _cacheOptionsSliding);
-
+                address = await SetAddressItemInCache(id);
             }
 
             return address;
         }
 
-        public async Task<Address> SetAddressItem(int id)
+        private async Task<Address> GetAddressFromCache(int id)
+        {
+            Address address = new Address();
+            string cachedAddress = await _cache.GetStringAsync(Constants.AppName + Constants.ApiVersion + "address" + id);
+            if (!string.IsNullOrEmpty(cachedAddress))
+            {
+                address = JsonConvert.DeserializeObject<Address>(cachedAddress);
+            }
+
+            return address;
+        }
+
+        private async Task<Address> SetAddressItemInCache(int id)
         {
             Address addressItem = await _context.AddressDb.AsNoTracking().SingleOrDefaultAsync(a => a.AddressId == id);
             await _cache.SetStringAsync(Constants.AppName + Constants.ApiVersion + "address" + id, JsonConvert.SerializeObject(addressItem), _cacheOptionsSliding);
@@ -137,32 +187,46 @@ namespace KinaUnaProgenyApi.Services
             _ = await _context.AddressDb.AddAsync(addressItem);
             _ = await _context.SaveChangesAsync();
 
-            _ = await SetAddressItem(addressItem.AddressId);
+            _ = await SetAddressItemInCache(addressItem.AddressId);
 
             return addressItem;
         }
 
         public async Task<Address> UpdateAddressItem(Address addressItem)
         {
-            _ = _context.AddressDb.Update(addressItem);
-            _ = await _context.SaveChangesAsync();
-
-            _ = await SetAddressItem(addressItem.AddressId);
-
+            Address addressToUpdate = await _context.AddressDb.SingleOrDefaultAsync(a => a.AddressId == addressItem.AddressId);
+            if (addressToUpdate != null)
+            {
+                addressToUpdate.PostalCode = addressItem.PostalCode;
+                addressToUpdate.Country = addressItem.Country;
+                addressToUpdate.City = addressItem.City;
+                addressToUpdate.AddressLine1 = addressItem.AddressLine1;
+                addressToUpdate.AddressLine2 = addressItem.AddressLine2;
+                addressToUpdate.State = addressItem.State;
+                
+                _ = _context.AddressDb.Update(addressToUpdate);
+                _ = await _context.SaveChangesAsync();
+                _ = await SetAddressItemInCache(addressItem.AddressId);
+            }
+            
             return addressItem;
         }
 
         public async Task RemoveAddressItem(int id)
         {
-            Address addressItem = await _context.AddressDb.SingleOrDefaultAsync(a => a.AddressId == id);
-            if (addressItem != null)
+            Address addressToRemove = await _context.AddressDb.SingleOrDefaultAsync(a => a.AddressId == id);
+            if (addressToRemove != null)
             {
-                _ = _context.AddressDb.Remove(addressItem);
+                _ = _context.AddressDb.Remove(addressToRemove);
                 _ = await _context.SaveChangesAsync();
 
-                await _cache.RemoveAsync(Constants.AppName + Constants.ApiVersion + "address" + id);
+                await RemoveAddressFromCache(id);
             }
-            
+        }
+
+        private async Task RemoveAddressFromCache(int id)
+        {
+            await _cache.RemoveAsync(Constants.AppName + Constants.ApiVersion + "address" + id);
         }
     }
 }
