0c1aeb09b58609751a27098b2f72335f6f3d69de
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2023-02-13 12:45:23 +0100

    Renamed _context to _progenyDbContext in DataService.cs.

--- KinaUnaProgenyApi/Services/DataService.cs
+++ KinaUnaProgenyApi/Services/DataService.cs
@@ -3,38 +3,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using KinaUna.Data.Contexts;
 using KinaUna.Data.Models;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace KinaUnaProgenyApi.Services
 {
     public class DataService: IDataService
     {
-        private readonly ProgenyDbContext _context;
+        private readonly ProgenyDbContext _progenyDbContext;
         private readonly WebDbContext _webDbContext;
-        public DataService(ProgenyDbContext context, WebDbContext webDbContext)
+        public DataService(ProgenyDbContext progenyDbContext, WebDbContext webDbContext)
         {
-            _context = context;
+            _progenyDbContext = progenyDbContext;
             _webDbContext = webDbContext;
         }
 
         public async Task<MobileNotification> GetMobileNotification(int id)
         {
-            MobileNotification notification = await _context.MobileNotificationsDb.SingleOrDefaultAsync(m => m.NotificationId == id);
+            MobileNotification notification = await _progenyDbContext.MobileNotificationsDb.SingleOrDefaultAsync(m => m.NotificationId == id);
             return notification;
         }
 
         public async Task<MobileNotification> AddMobileNotification(MobileNotification notification)
         {
-            _ = _context.MobileNotificationsDb.Add(notification);
-            _ = await _context.SaveChangesAsync();
+            _ = _progenyDbContext.MobileNotificationsDb.Add(notification);
+            _ = await _progenyDbContext.SaveChangesAsync();
 
             return notification;
         }
 
         public async Task<MobileNotification> UpdateMobileNotification(MobileNotification notification)
         {
-            MobileNotification updatedNotification = await _context.MobileNotificationsDb.SingleOrDefaultAsync(mn => mn.NotificationId == notification.NotificationId);
+            MobileNotification updatedNotification = await _progenyDbContext.MobileNotificationsDb.SingleOrDefaultAsync(mn => mn.NotificationId == notification.NotificationId);
             if (updatedNotification != null)
             {
                 updatedNotification.IconLink = notification.IconLink;
@@ -47,8 +46,8 @@ namespace KinaUnaProgenyApi.Services
                 updatedNotification.Time = notification.Time;
                 updatedNotification.UserId = notification.UserId;
                 
-                _ = _context.MobileNotificationsDb.Update(updatedNotification);
-                _ = await _context.SaveChangesAsync();
+                _ = _progenyDbContext.MobileNotificationsDb.Update(updatedNotification);
+                _ = await _progenyDbContext.SaveChangesAsync();
             }
             
             return notification;
@@ -56,21 +55,21 @@ namespace KinaUnaProgenyApi.Services
 
         public async Task<MobileNotification> DeleteMobileNotification(MobileNotification notification)
         {
-            MobileNotification notificationToDelete = await _context.MobileNotificationsDb.SingleOrDefaultAsync(mn => mn.NotificationId == notification.NotificationId);
+            MobileNotification notificationToDelete = await _progenyDbContext.MobileNotificationsDb.SingleOrDefaultAsync(mn => mn.NotificationId == notification.NotificationId);
             if (notificationToDelete != null)
             {
-                _ = _context.MobileNotificationsDb.Remove(notificationToDelete);
-                _ = await _context.SaveChangesAsync();
+                _ = _progenyDbContext.MobileNotificationsDb.Remove(notificationToDelete);
+                _ = await _progenyDbContext.SaveChangesAsync();
             }
             
             return notification;
         }
         public async Task<List<MobileNotification>> GetUsersMobileNotifications(string userId, string language)
         {
-            List<MobileNotification> notifications = await _context.MobileNotificationsDb.Where(n => n.UserId == userId && n.Language.ToUpper() == language.ToUpper()).ToListAsync();
+            List<MobileNotification> notifications = await _progenyDbContext.MobileNotificationsDb.Where(n => n.UserId == userId && n.Language.ToUpper() == language.ToUpper()).ToListAsync();
             if (string.IsNullOrEmpty(language))
             {
-                notifications = await _context.MobileNotificationsDb.Where(n => n.UserId == userId).ToListAsync();
+                notifications = await _progenyDbContext.MobileNotificationsDb.Where(n => n.UserId == userId).ToListAsync();
             }
 
             return notifications;
