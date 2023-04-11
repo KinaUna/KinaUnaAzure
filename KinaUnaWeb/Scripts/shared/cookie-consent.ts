2b8b40fd63ac6666ed3a56cbf8e85c877745cb52
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2023-04-11 20:29:44 +0200

    Moved inline JavaScript from Web/Views/Shared/_CookieConsentPartial.cshtml to TypeScript file Web/Scripts/shared/cookie-consent.ts.

---
+++ KinaUnaWeb/Scripts/shared/cookie-consent.ts
@@ -0,0 +1,29 @@
+﻿declare var cookieDomainName: string;
+$(function (): void {
+    const saveGdprButton = document.querySelector<HTMLButtonElement>('#saveGdprButton');
+    const gdprAllowMapsSwitch = document.querySelector<HTMLInputElement>('#allowMapsSwitch');
+    const gdprAllowYoutubeSwitch = document.querySelector<HTMLInputElement>('#allowYouTubeSwitch');
+
+    if (saveGdprButton !== null) {
+        saveGdprButton.addEventListener('click',
+            function () {
+                let gdprSettings = 'Essential';
+
+                if (gdprAllowMapsSwitch !== null && gdprAllowMapsSwitch.checked) {
+                    gdprSettings = gdprSettings + 'HereMaps';
+                }
+
+                if (gdprAllowYoutubeSwitch !== null && gdprAllowYoutubeSwitch.checked) {
+                    gdprSettings = gdprSettings + 'YouTube';
+                }
+
+                let date = new Date();
+                date.setTime(date.getTime() + (180 * 24 * 60 * 60 * 1000));
+                let expires = '; expires=' + date.toUTCString();
+                document.cookie = 'KinaUnaConsent=' + gdprSettings + expires + ';' + cookieDomainName;
+                location.reload();
+            },
+            false);
+    }
+    
+});
