45be4aaf269d5419206ce55712f2081fd8451dbd
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-07-18 15:21:08 +0200

    Added new migration for ApplicationDbContext after removing DbSet for DataProtectionKeys.
    Removed ApplicationDbContext service from Web/Startup.cs.

---
+++ KinaUna.IDP/Migrations/20240718130659_RemovedDataProtectionKeys.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KinaUna.IDP.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedDataProtectionKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataProtectionKeys");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataProtectionKeys",
+                columns: table => new
+                {
+                    FriendlyName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    XmlData = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataProtectionKeys", x => x.FriendlyName);
+                });
+        }
+    }
+}
