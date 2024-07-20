01d3e036b542a4e1157d9e934698629630c6d519
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-07-20 15:30:07 +0200

    Data/Extensions/FriendExtensions.cs documentation: Added summary comments for extention methods.

--- KinaUna.Data/Extensions/FriendExtensions.cs
+++ KinaUna.Data/Extensions/FriendExtensions.cs
@@ -3,8 +3,16 @@ using System;
 
 namespace KinaUna.Data.Extensions
 {
+    /// <summary>
+    /// Extension methods for the Friend class.
+    /// </summary>
     public static class FriendExtensions
     {
+        /// <summary>
+        /// Copies the properties needed for updating a Friend entity from one Friend object to another.
+        /// </summary>
+        /// <param name="currentFriend"></param>
+        /// <param name="otherFriend"></param>
         public static void CopyPropertiesForUpdate(this Friend currentFriend, Friend otherFriend )
         {
             currentFriend.AccessLevel = otherFriend.AccessLevel;
@@ -24,6 +32,11 @@ namespace KinaUna.Data.Extensions
             currentFriend.Tags = otherFriend.Tags;
         }
 
+        /// <summary>
+        /// Copies the properties needed for adding a Friend entity from one Friend object to another.
+        /// </summary>
+        /// <param name="currentFriend"></param>
+        /// <param name="otherFriend"></param>
         public static void CopyPropertiesForAdd(this Friend currentFriend, Friend otherFriend)
         {
             currentFriend.AccessLevel = otherFriend.AccessLevel;
@@ -40,6 +53,11 @@ namespace KinaUna.Data.Extensions
             currentFriend.Tags = otherFriend.Tags;
         }
 
+        /// <summary>
+        /// Produces a URL for the profile picture of a Friend object.
+        /// </summary>
+        /// <param name="friend"></param>
+        /// <returns>String with the URL</returns>
         public static string GetProfilePictureUrl(this Friend friend)
         {
             if (friend == null || friend.PictureLink == null)
@@ -57,7 +75,11 @@ namespace KinaUna.Data.Extensions
             return pictureUrl;
         }
 
-
+        /// <summary>
+        /// Generates a string with the MIME type for the profile picture of a Friend object, based on the file extension.
+        /// </summary>
+        /// <param name="friend"></param>
+        /// <returns>String with the MIME content type</returns>
         public static string GetPictureFileContentType(this Friend friend)
         {
             string contentType = FileContentTypeHelpers.GetContentTypeString(friend.PictureLink);
