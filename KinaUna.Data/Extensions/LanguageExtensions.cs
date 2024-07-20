95c7d8704d2b83bff1c17a5a91e9cd44483332bc
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-07-20 15:57:34 +0200

    Data/Extensions/LanguageExtensions.cs documentation updated: Added summary comments.

--- KinaUna.Data/Extensions/LanguageExtensions.cs
+++ KinaUna.Data/Extensions/LanguageExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace KinaUna.Data.Extensions
 {
+    /// <summary>
+    /// Extension methods for the KinaUnaLanguage class.
+    /// </summary>
     public static class LanguageExtensions
     {
+        /// <summary>
+        /// Gets the long format of the language code.
+        /// </summary>
+        /// <param name="language"></param>
+        /// <returns>string with the long format language code.</returns>
         public static string CodeToLongFormat(this KinaUnaLanguage language)
         {
             if (language.Code == "da")
