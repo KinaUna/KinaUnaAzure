f7407e6df9b972639491979be9bac3c831195204
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-07-22 13:41:47 +0200

    Documentation update.
    Added summary comments for actions in ProgenyApi/Controllers/AddressesControler.cs.

--- KinaUnaProgenyApi/Controllers/AddressesController.cs
+++ KinaUnaProgenyApi/Controllers/AddressesController.cs
@@ -14,6 +14,11 @@ namespace KinaUnaProgenyApi.Controllers
     {
         // Todo: Security check, verify that users can view/change addresses.
 
+        /// <summary>
+        /// Retrieves the Address with a given id.
+        /// </summary>
+        /// <param name="id">The id of the Address entity to retrieve.</param>
+        /// <returns>Address entity with the id.</returns>
         // GET api/addresses/5
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetAddressItem(int id)
@@ -22,6 +27,11 @@ namespace KinaUnaProgenyApi.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Adds a new Address entity to the database.
+        /// </summary>
+        /// <param name="value">The Address object to add.</param>
+        /// <returns>The added Address object</returns>
         // POST api/addresses
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Address value)
@@ -31,6 +41,12 @@ namespace KinaUnaProgenyApi.Controllers
             return Ok(addressItem);
         }
 
+        /// <summary>
+        /// Updates an existing Address entity in the database.
+        /// </summary>
+        /// <param name="id">The id of the Address to update.</param>
+        /// <param name="value">The Address object containing the properties to update.</param>
+        /// <returns>The updated Address object.</returns>
         // PUT api/addresses/5
         [HttpPut("{id:int}")]
         public async Task<IActionResult> Put(int id, [FromBody] Address value)
@@ -46,6 +62,11 @@ namespace KinaUnaProgenyApi.Controllers
             return Ok(addressItem);
         }
 
+        /// <summary>
+        /// Deletes an Address entity from the database.
+        /// </summary>
+        /// <param name="id">The id of the Address entity.</param>
+        /// <returns>NoContentResult.</returns>
         // DELETE api/addresses/5
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> Delete(int id)
