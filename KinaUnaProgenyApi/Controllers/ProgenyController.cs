7601793bec897440d7bb97e2faa20ba05260ebed
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-08 21:32:55 +0200

    Documentation update. Added summary comments for actions in ProgenyApi/Controllers/ProgenyController.cs

--- KinaUnaProgenyApi/Controllers/ProgenyController.cs
+++ KinaUnaProgenyApi/Controllers/ProgenyController.cs
@@ -15,6 +15,12 @@ namespace KinaUnaProgenyApi.Controllers
     [ApiController]
     public class ProgenyController(IImageStore imageStore, IProgenyService progenyService, IUserAccessService userAccessService) : ControllerBase
     {
+        /// <summary>
+        /// Gets a list of all Progeny that the current user is admin for.
+        /// Obsolete: this action will show the user's email in the url.
+        /// </summary>
+        /// <param name="id">The user's email address.</param>
+        /// <returns>List of Progeny.</returns>
         // GET api/progeny/parent/[id]
         [HttpGet]
         [Route("[action]/{id}")]
@@ -33,6 +39,11 @@ namespace KinaUnaProgenyApi.Controllers
 
         }
 
+        /// <summary>
+        /// Gets the Progeny entity with the specified id.
+        /// </summary>
+        /// <param name="id">The ProgenyId for the Progeny entity to get.</param>
+        /// <returns>The Progeny object with the given ProgenyId.</returns>
         // GET api/progeny/5
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetProgeny(int id)
@@ -51,6 +62,12 @@ namespace KinaUnaProgenyApi.Controllers
 
         }
 
+        /// <summary>
+        /// Gets the Progeny entity with the specified id.
+        /// For mobile clients.
+        /// </summary>
+        /// <param name="id">The ProgenyId of the Progeny to get.</param>
+        /// <returns>Progeny object with the given ProgenyId.</returns>
         // For Xamarin mobile app.
         [HttpGet("[action]/{id:int}")]
         public async Task<IActionResult> Mobile(int id)
@@ -65,6 +82,11 @@ namespace KinaUnaProgenyApi.Controllers
 
         }
 
+        /// <summary>
+        /// Adds a new Progeny entity to the database.
+        /// </summary>
+        /// <param name="value">Progeny object to add to the database.</param>
+        /// <returns>The added Progeny object.</returns>
         // POST api/progeny
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Progeny value)
@@ -127,6 +149,12 @@ namespace KinaUnaProgenyApi.Controllers
             return Ok(progeny);
         }
 
+        /// <summary>
+        /// Updates the Progeny entity with the specified id.
+        /// </summary>
+        /// <param name="id">The ProgenyId of the Progeny to update.</param>
+        /// <param name="value">Progeny object with the updated properties.</param>
+        /// <returns>The updated Progeny object.</returns>
         // PUT api/progeny/5
         [HttpPut("{id:int}")]
         public async Task<IActionResult> Put(int id, [FromBody] Progeny value)
@@ -201,6 +229,11 @@ namespace KinaUnaProgenyApi.Controllers
             return Ok(progeny);
         }
 
+        /// <summary>
+        /// Deletes the Progeny entity with the specified id.
+        /// </summary>
+        /// <param name="id">The ProgenyId of the entity to delete.</param>
+        /// <returns>NoContentResult.</returns>
         // DELETE api/progeny/5
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> Delete(int id)
