9b61760c0fc768890a726bfb9bfafb0449b90a71
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-12 15:40:33 +0200

    Documentation update. Added summary comments for methods in ProgenyApi/Services/IAzureNotifications.cs and ProgenyApi/Services/AzureNotifications.cs

--- KinaUnaProgenyApi/Services/AzureNotifications.cs
+++ KinaUnaProgenyApi/Services/AzureNotifications.cs
@@ -8,12 +8,28 @@ using Newtonsoft.Json.Linq;
 
 namespace KinaUnaProgenyApi.Services
 {
+    /// <summary>
+    /// Implementation of the IAzureNotifications interface, used to manage mobile notifications.
+    /// </summary>
+    /// <param name="configuration"></param>
+    /// <param name="dataService"></param>
+    /// <param name="userInfoService"></param>
+    /// <param name="userAccessService"></param>
     public class AzureNotifications(IConfiguration configuration, IDataService dataService, IUserInfoService userInfoService, IUserAccessService userAccessService)
         : IAzureNotifications
     {
         public NotificationHubClient Hub { get; set; } = NotificationHubClient.CreateClientFromConnectionString(configuration["NotificationHubConnection"],
             "kinaunanotifications");
 
+        /// <summary>
+        /// Sends a notification to all users with access to a TimeLineItem.
+        /// For use when a TimeLineItem is created or updated.
+        /// Also saves the notification in the database for later retrieval.
+        /// </summary>
+        /// <param name="title">The title of the notification.</param>
+        /// <param name="message">The body/content of the message.</param>
+        /// <param name="timeLineItem">The TimeLineItem the notification is for.</param>
+        /// <param name="iconLink">Link to the image used as icon when the notification is displayed in the notification history.</param>
         public async Task ProgenyUpdateNotification(string title, string message, TimeLineItem timeLineItem, string iconLink = "")
         {
 
--- KinaUnaProgenyApi/Services/IAzureNotifications.cs
+++ KinaUnaProgenyApi/Services/IAzureNotifications.cs
@@ -6,6 +6,19 @@ namespace KinaUnaProgenyApi.Services;
 
 public interface IAzureNotifications
 {
+    /// <summary>
+    /// Interface for dependency injection of the AzureNotifications class, used to manage mobile notifications.
+    /// </summary>
     NotificationHubClient Hub { get; set; }
+
+    /// <summary>
+    /// Sends a notification to all users with access to a TimeLineItem.
+    /// For use when a TimeLineItem is created or updated.
+    /// Also saves the notification in the database for later retrieval.
+    /// </summary>
+    /// <param name="title">The title of the notification.</param>
+    /// <param name="message">The body/content of the message.</param>
+    /// <param name="timeLineItem">The TimeLineItem the notification is for.</param>
+    /// <param name="iconLink">Link to the image used as icon when the notification is displayed in the notification history.</param>
     Task ProgenyUpdateNotification(string title, string message, TimeLineItem timeLineItem, string iconLink = "");
 }
