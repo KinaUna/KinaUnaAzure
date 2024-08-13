32da5479441cec4a547ac43ce7934d1629012542
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-13 14:58:52 +0200

    Documentation update. Added summary comments for methods in ProgenyApi/Services/ILanguageService.cs and ProgenyApi/Services/LanguageService.cs

--- KinaUnaProgenyApi/Services/ILanguageService.cs
+++ KinaUnaProgenyApi/Services/ILanguageService.cs
@@ -6,10 +6,38 @@ namespace KinaUnaProgenyApi.Services
 {
     public interface ILanguageService
     {
+        /// <summary>
+        /// Gets a list of all KinaUnaLanguages.
+        /// </summary>
+        /// <returns>List of KinaUnaLanguage</returns>
         Task<List<KinaUnaLanguage>> GetAllLanguages();
+
+        /// <summary>
+        /// Gets a KinaUnaLanguage by Id.
+        /// </summary>
+        /// <param name="languageId">The Id of the KinaUnaLanguage.</param>
+        /// <returns>KinaUnaLanguage with the given Id.</returns>
         Task<KinaUnaLanguage> GetLanguage(int languageId);
+
+        /// <summary>
+        /// Adds a new KinaUnaLanguage to the database.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage to add.</param>
+        /// <returns>The added KinaUnaLanguage.</returns>
         Task<KinaUnaLanguage> AddLanguage(KinaUnaLanguage language);
+
+        /// <summary>
+        /// Updates a KinaUnaLanguage in the database.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object with updated properties.</param>
+        /// <returns>The updated KinaUnaLanguage object.</returns>
         Task<KinaUnaLanguage> UpdateLanguage(KinaUnaLanguage language);
+
+        /// <summary>
+        /// Deletes a KinaUnaLanguage from the database.
+        /// </summary>
+        /// <param name="languageId">The Id of the KinaUnaLanguage to delete.</param>
+        /// <returns>The deleted KinaUnaLanguage.</returns>
         Task<KinaUnaLanguage> DeleteLanguage(int languageId);
     }
 }
--- KinaUnaProgenyApi/Services/LanguageService.cs
+++ KinaUnaProgenyApi/Services/LanguageService.cs
@@ -8,6 +8,10 @@ namespace KinaUnaProgenyApi.Services
 {
     public class LanguageService(ProgenyDbContext context) : ILanguageService
     {
+        /// <summary>
+        /// Gets a list of all KinaUnaLanguages.
+        /// </summary>
+        /// <returns>List of KinaUnaLanguage</returns>
         public async Task<List<KinaUnaLanguage>> GetAllLanguages()
         {
             List<KinaUnaLanguage> languagesList = await context.Languages.AsNoTracking().ToListAsync();
@@ -20,12 +24,22 @@ namespace KinaUnaProgenyApi.Services
             return languagesList;
         }
 
+        /// <summary>
+        /// Gets a KinaUnaLanguage by Id.
+        /// </summary>
+        /// <param name="languageId">The Id of the KinaUnaLanguage.</param>
+        /// <returns>KinaUnaLanguage with the given Id.</returns>
         public async Task<KinaUnaLanguage> GetLanguage(int languageId)
         {
             KinaUnaLanguage language = await context.Languages.AsNoTracking().SingleOrDefaultAsync(l => l.Id == languageId);
             return language;
         }
 
+        /// <summary>
+        /// Adds a new KinaUnaLanguage to the database.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage to add.</param>
+        /// <returns>The added KinaUnaLanguage.</returns>
         public async Task<KinaUnaLanguage> AddLanguage(KinaUnaLanguage language)
         {
             _ = context.Languages.Add(language);
@@ -33,6 +47,11 @@ namespace KinaUnaProgenyApi.Services
             return language;
         }
 
+        /// <summary>
+        /// Updates a KinaUnaLanguage in the database.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object with updated properties.</param>
+        /// <returns>The updated KinaUnaLanguage object.</returns>
         public async Task<KinaUnaLanguage> UpdateLanguage(KinaUnaLanguage language)
         {
             KinaUnaLanguage languageToUpdate = await context.Languages.SingleOrDefaultAsync(l => l.Id == language.Id);
@@ -49,6 +68,11 @@ namespace KinaUnaProgenyApi.Services
             return language;
         }
 
+        /// <summary>
+        /// Deletes a KinaUnaLanguage from the database.
+        /// </summary>
+        /// <param name="languageId">The Id of the KinaUnaLanguage to delete.</param>
+        /// <returns>The deleted KinaUnaLanguage.</returns>
         public async Task<KinaUnaLanguage> DeleteLanguage(int languageId)
         {
             KinaUnaLanguage language = await context.Languages.SingleOrDefaultAsync(l => l.Id == languageId);
