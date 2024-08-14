0e0a8485d9ec390fe88935a0c871bfcd12104567
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-14 12:20:07 +0200

    Documentation update. Added summary comments for methods in ProgenyApi/Services/ISkillsService.cs and ProgenyApi/Services/SkillsService.cs

--- KinaUnaProgenyApi/Services/ISkillService.cs
+++ KinaUnaProgenyApi/Services/ISkillService.cs
@@ -6,10 +6,41 @@ namespace KinaUnaProgenyApi.Services
 {
     public interface ISkillService
     {
+        /// <summary>
+        /// Gets the Skill with the specified SkillId.
+        /// First checks the cache, if not found, gets the Skill from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="id">The SkillId of the Skill to get.</param>
+        /// <returns>The Skill object with the given SkillId. Null if it doesn't exist.</returns>
         Task<Skill> GetSkill(int id);
+
+        /// <summary>
+        /// Adds a new Skill to the database and adds it to the cache.
+        /// </summary>
+        /// <param name="skill">The Skill object to add.</param>
+        /// <returns>The added Skill object.</returns>
         Task<Skill> AddSkill(Skill skill);
+
+        /// <summary>
+        /// Updates a Skill in the database and the cache.
+        /// </summary>
+        /// <param name="skill">The SKill object with the updated properties.</param>
+        /// <returns>The updated Skill object.</returns>
         Task<Skill> UpdateSkill(Skill skill);
+
+        /// <summary>
+        /// Deletes a Skill from the database and the cache.
+        /// </summary>
+        /// <param name="skill">The Skill object to delete.</param>
+        /// <returns>The deleted Skill object.</returns>
         Task<Skill> DeleteSkill(Skill skill);
+
+        /// <summary>
+        /// Gets a list of all Skills for a Progeny from the cache.
+        /// If the list is empty, gets the list from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get Skills for.</param>
+        /// <returns>List of Skill objects.</returns>
         Task<List<Skill>> GetSkillsList(int progenyId);
     }
 }
--- KinaUnaProgenyApi/Services/SkillService.cs
+++ KinaUnaProgenyApi/Services/SkillService.cs
@@ -26,6 +26,12 @@ namespace KinaUnaProgenyApi.Services
             _cacheOptionsSliding.SetSlidingExpiration(new System.TimeSpan(7, 0, 0, 0)); // Expire after a week.
         }
 
+        /// <summary>
+        /// Gets the Skill with the specified SkillId.
+        /// First checks the cache, if not found, gets the Skill from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="id">The SkillId of the Skill to get.</param>
+        /// <returns>The Skill object with the given SkillId. Null if it doesn't exist.</returns>
         public async Task<Skill> GetSkill(int id)
         {
             Skill skill = await GetSkillFromCache(id);
@@ -36,6 +42,11 @@ namespace KinaUnaProgenyApi.Services
             return skill;
         }
 
+        /// <summary>
+        /// Adds a new Skill to the database and adds it to the cache.
+        /// </summary>
+        /// <param name="skill">The Skill object to add.</param>
+        /// <returns>The added Skill object.</returns>
         public async Task<Skill> AddSkill(Skill skill)
         {
             Skill skillToAdd = new();
@@ -47,18 +58,29 @@ namespace KinaUnaProgenyApi.Services
             return skillToAdd;
         }
 
+        /// <summary>
+        /// Gets the Skill with the specified SkillId from the cache.
+        /// </summary>
+        /// <param name="id">The SkillId of the Skill to get.</param>
+        /// <returns>The SKill with the given SKillId. Null if the Skill isn't found in the cache.</returns>
         private async Task<Skill> GetSkillFromCache(int id)
         {
-            Skill skill = new();
             string cachedSkill = await _cache.GetStringAsync(Constants.AppName + Constants.ApiVersion + "skill" + id);
-            if (!string.IsNullOrEmpty(cachedSkill))
+            if (string.IsNullOrEmpty(cachedSkill))
             {
-                skill = JsonConvert.DeserializeObject<Skill>(cachedSkill);
+                return null;
             }
 
+            Skill skill = JsonConvert.DeserializeObject<Skill>(cachedSkill);
             return skill;
         }
 
+        /// <summary>
+        /// Gets the Skill with the specified SkillId from the database and adds it to the cache.
+        /// Also updates the SkillsList for the Progeny in the cache.
+        /// </summary>
+        /// <param name="id">The SkillId of the Skill to get and set.</param>
+        /// <returns>The SKill with the given SkillId. Null if the Skill doesn't exist.</returns>
         private async Task<Skill> SetSkillInCache(int id)
         {
             Skill skill = await _context.SkillsDb.AsNoTracking().SingleOrDefaultAsync(s => s.SkillId == id);
@@ -71,6 +93,11 @@ namespace KinaUnaProgenyApi.Services
             return skill;
         }
 
+        /// <summary>
+        /// Updates a Skill in the database and the cache.
+        /// </summary>
+        /// <param name="skill">The SKill object with the updated properties.</param>
+        /// <returns>The updated Skill object.</returns>
         public async Task<Skill> UpdateSkill(Skill skill)
         {
             Skill skillToUpdate = await _context.SkillsDb.SingleOrDefaultAsync(s => s.SkillId == skill.SkillId);
@@ -86,6 +113,11 @@ namespace KinaUnaProgenyApi.Services
             return skillToUpdate;
         }
 
+        /// <summary>
+        /// Deletes a Skill from the database and the cache.
+        /// </summary>
+        /// <param name="skill">The Skill object to delete.</param>
+        /// <returns>The deleted Skill object.</returns>
         public async Task<Skill> DeleteSkill(Skill skill)
         {
             Skill skillToDelete = await _context.SkillsDb.SingleOrDefaultAsync(s => s.SkillId == skill.SkillId);
@@ -98,6 +130,12 @@ namespace KinaUnaProgenyApi.Services
             return skillToDelete;
         }
 
+        /// <summary>
+        /// Removes a Skill from the cache and updates the SkillsList for the Progeny in the cache.
+        /// </summary>
+        /// <param name="id">The SkillId of the Skill to remove.</param>
+        /// <param name="progenyId">The ProgenyId of the Progeny that the Skill belongs to.</param>
+        /// <returns></returns>
         private async Task RemoveSkillFromCache(int id, int progenyId)
         {
             await _cache.RemoveAsync(Constants.AppName + Constants.ApiVersion + "skill" + id);
@@ -105,6 +143,12 @@ namespace KinaUnaProgenyApi.Services
             _ = await SetSkillsListInCache(progenyId);
         }
 
+        /// <summary>
+        /// Gets a list of all Skills for a Progeny from the cache.
+        /// If the list is empty, gets the list from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get Skills for.</param>
+        /// <returns>List of Skill objects.</returns>
         public async Task<List<Skill>> GetSkillsList(int progenyId)
         {
             List<Skill> skillsList = await GetSkillsListFromCache(progenyId);
@@ -116,6 +160,11 @@ namespace KinaUnaProgenyApi.Services
             return skillsList;
         }
 
+        /// <summary>
+        /// Gets a list of all Skills for a Progeny from the cache.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get Skills for.</param>
+        /// <returns>List of Skill objects.</returns>
         private async Task<List<Skill>> GetSkillsListFromCache(int progenyId)
         {
             List<Skill> skillsList = [];
@@ -128,6 +177,11 @@ namespace KinaUnaProgenyApi.Services
             return skillsList;
         }
 
+        /// <summary>
+        /// Gets a list of all Skills for a Progeny from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get Skills for.</param>
+        /// <returns>List of SKill objects.</returns>
         private async Task<List<Skill>> SetSkillsListInCache(int progenyId)
         {
             List<Skill> skillsList = await _context.SkillsDb.AsNoTracking().Where(s => s.ProgenyId == progenyId).ToListAsync();
