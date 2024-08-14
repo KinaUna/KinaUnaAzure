b9c12c96377c8ffd39a4e9d7f29b21919df00210
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-14 14:47:53 +0200

    Documentation update. Added summary comments for methods in ProgenyApi/Services/ITextTranslationService.cs and ProgenyApi/Services/TextTranslationService.cs

--- KinaUnaProgenyApi/Services/ITextTranslationService.cs
+++ KinaUnaProgenyApi/Services/ITextTranslationService.cs
@@ -6,13 +6,66 @@ namespace KinaUnaProgenyApi.Services
 {
     public interface ITextTranslationService
     {
+        /// <summary>
+        /// Gets a list of all TextTranslations.
+        /// </summary>
+        /// <param name="languageId">The LanguageId to get translations in.</param>
+        /// <returns>List of TextTranslation objects.</returns>
         Task<List<TextTranslation>> GetAllTranslations(int languageId);
+
+        /// <summary>
+        /// Gets a TextTranslation with the specified Id.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to get.</param>
+        /// <returns>TextTranslation with the given Id. Null if the TextTranslation doesn't exist.</returns>
         Task<TextTranslation> GetTranslationById(int id);
+
+        /// <summary>
+        /// Gets a TextTranslation by word, page and language.
+        /// </summary>
+        /// <param name="word">The word to get a translation for.</param>
+        /// <param name="page">The page the word appears on.</param>
+        /// <param name="languageId">The language the text is viewed in.</param>
+        /// <returns>TextTranslation object. Null if the TextTranslation doesn't exist.</returns>
         Task<TextTranslation> GetTranslationByWord(string word, string page, int languageId);
+
+        /// <summary>
+        /// Gets a list of all TextTranslations for a specific page.
+        /// </summary>
+        /// <param name="languageId">The LanguageId of the language the text is viewed in.</param>
+        /// <param name="pageName">The page the texts appear on.</param>
+        /// <returns>List of TextTranslation objects.</returns>
         Task<List<TextTranslation>> GetPageTranslations(int languageId, string pageName);
+
+        /// <summary>
+        /// Adds a new TextTranslation to the database.
+        /// </summary>
+        /// <param name="translation">The TextTranslation object to add.</param>
+        /// <returns>The added TextTranslation object.</returns>
         Task<TextTranslation> AddTranslation(TextTranslation translation);
+
+        /// <summary>
+        /// Updates a TextTranslation in the database.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to update.</param>
+        /// <param name="translation">The TextTranslation object with the updated properties.</param>
+        /// <returns>The updated TextTranslation object.</returns>
         Task<TextTranslation> UpdateTranslation(int id, TextTranslation translation);
+
+        /// <summary>
+        /// Deletes a TextTranslation from the database.
+        /// Also deletes translations in all other languages for the same word and page.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to delete.</param>
+        /// <returns>The deleted TextTranslation object.</returns>
         Task<TextTranslation> DeleteTranslation(int id);
+
+        /// <summary>
+        /// Deletes a single translation from the database.
+        /// Doesn't delete other language translations for the same word and page.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to delete.</param>
+        /// <returns>The deleted TextTranslation object.</returns>
         Task<TextTranslation> DeleteSingleTranslation(int id);
     }
 }
--- KinaUnaProgenyApi/Services/TextTranslationService.cs
+++ KinaUnaProgenyApi/Services/TextTranslationService.cs
@@ -9,6 +9,11 @@ namespace KinaUnaProgenyApi.Services
 {
     public class TextTranslationService(ProgenyDbContext context) : ITextTranslationService
     {
+        /// <summary>
+        /// Gets a list of all TextTranslations.
+        /// </summary>
+        /// <param name="languageId">The LanguageId to get translations in.</param>
+        /// <returns>List of TextTranslation objects.</returns>
         public async Task<List<TextTranslation>> GetAllTranslations(int languageId)
         {
             List<TextTranslation> translations;
@@ -24,12 +29,23 @@ namespace KinaUnaProgenyApi.Services
             return translations;
         }
 
+        /// <summary>
+        /// Gets a TextTranslation with the specified Id.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to get.</param>
+        /// <returns>TextTranslation with the given Id. Null if the TextTranslation doesn't exist.</returns>
         public async Task<TextTranslation> GetTranslationById(int id)
         {
             TextTranslation translation = await context.TextTranslations.AsNoTracking().SingleOrDefaultAsync(t => t.Id == id);
             return translation;
         }
 
+        /// <summary>
+        /// Gets a list of all TextTranslations for a specific page.
+        /// </summary>
+        /// <param name="languageId">The LanguageId of the language the text is viewed in.</param>
+        /// <param name="pageName">The page the texts appear on.</param>
+        /// <returns>List of TextTranslation objects.</returns>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1862:Use the 'StringComparison' method overloads to perform case-insensitive string comparisons", Justification = "StringComparison seems to break Db queries.")]
         public async Task<List<TextTranslation>> GetPageTranslations(int languageId, string pageName)
         {
@@ -38,6 +54,13 @@ namespace KinaUnaProgenyApi.Services
             return translations;
         }
 
+        /// <summary>
+        /// Gets a TextTranslation by word, page and language.
+        /// </summary>
+        /// <param name="word">The word to get a translation for.</param>
+        /// <param name="page">The page the word appears on.</param>
+        /// <param name="languageId">The language the text is viewed in.</param>
+        /// <returns>TextTranslation object. Null if the TextTranslation doesn't exist.</returns>
         public async Task<TextTranslation> GetTranslationByWord(string word, string page, int languageId)
         {
             if (languageId == 0)
@@ -49,6 +72,11 @@ namespace KinaUnaProgenyApi.Services
             return textTranslation;
         }
 
+        /// <summary>
+        /// Adds a new TextTranslation to the database.
+        /// </summary>
+        /// <param name="translation">The TextTranslation object to add.</param>
+        /// <returns>The added TextTranslation object.</returns>
         public async Task<TextTranslation> AddTranslation(TextTranslation translation)
         {
             _ = context.TextTranslations.Add(translation);
@@ -59,6 +87,11 @@ namespace KinaUnaProgenyApi.Services
             return translation;
         }
 
+        /// <summary>
+        /// Checks if a TextTranslation has translated version in each language, and adds any missing entries.
+        /// </summary>
+        /// <param name="translation">The TextTranslation to check for.</param>
+        /// <returns></returns>
         private async Task AddMissingLanguagesForTranslation(TextTranslation translation)
         {
             List<KinaUnaLanguage> languages = await context.Languages.AsNoTracking().ToListAsync();
@@ -81,6 +114,12 @@ namespace KinaUnaProgenyApi.Services
             }
         }
 
+        /// <summary>
+        /// Updates a TextTranslation in the database.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to update.</param>
+        /// <param name="translation">The TextTranslation object with the updated properties.</param>
+        /// <returns>The updated TextTranslation object.</returns>
         public async Task<TextTranslation> UpdateTranslation(int id, TextTranslation translation)
         {
             TextTranslation translationItem = await context.TextTranslations.SingleOrDefaultAsync(t => t.Id == id);
@@ -93,6 +132,12 @@ namespace KinaUnaProgenyApi.Services
             return translationItem;
         }
 
+        /// <summary>
+        /// Deletes a TextTranslation from the database.
+        /// Also deletes translations in all other languages for the same word and page.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to delete.</param>
+        /// <returns>The deleted TextTranslation object.</returns>
         public async Task<TextTranslation> DeleteTranslation(int id)
         {
             TextTranslation translation = await context.TextTranslations.AsNoTracking().SingleOrDefaultAsync(t => t.Id == id);
@@ -113,6 +158,12 @@ namespace KinaUnaProgenyApi.Services
             return translation;
         }
 
+        /// <summary>
+        /// Deletes a single translation from the database.
+        /// Doesn't delete other language translations for the same word and page.
+        /// </summary>
+        /// <param name="id">The Id of the TextTranslation to delete.</param>
+        /// <returns>The deleted TextTranslation object.</returns>
         public async Task<TextTranslation> DeleteSingleTranslation(int id)
         {
             TextTranslation translation = await context.TextTranslations.SingleOrDefaultAsync(t => t.Id == id);
