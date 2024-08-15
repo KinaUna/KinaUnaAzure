5c403fc5e8968b02554a8eb189d063314bfe197a
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-15 12:44:29 +0200

    Documentation update. Added summary comments for methods in ProgenyApi/Services/IVideosService.cs and ProgenyApi/Services/VideosService.cs

--- KinaUnaProgenyApi/Services/IVideosService.cs
+++ KinaUnaProgenyApi/Services/IVideosService.cs
@@ -6,14 +6,71 @@ namespace KinaUnaProgenyApi.Services
 {
     public interface IVideosService
     {
+        /// <summary>
+        /// Gets a Video entity with the specified VideoId.
+        /// First checks the cache, if not found, gets the Video from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="id">The VideoId of the Video to get.</param>
+        /// <returns>The Video object with the given VideoId. Null if the Video item doesn't exist.</returns>
         Task<Video> GetVideo(int id);
+
+        /// <summary>
+        /// Gets a Video entity with the specified VideoLink.
+        /// </summary>
+        /// <param name="link">The VideoLink of the Video item to get.</param>
+        /// <param name="progenyId">The ProgenyId of the Video item to get.</param>
+        /// <returns>The Video with the given VideoLink and ProgenyId. Null if the Video doesn't exist.</returns>
         Task<Video> GetVideoByLink(string link, int progenyId);
+
+        /// <summary>
+        /// Gets a Video entity with the specified VideoId from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="id">The VideoId of the Video to get and set.</param>
+        /// <returns>The Video object with the given VideoId. Null if the Video doesn't exist.</returns>
         Task<Video> SetVideoInCache(int id);
+
+        /// <summary>
+        /// Adds a new Video entity to the database and adds it to the cache.
+        /// </summary>
+        /// <param name="video">The Video object to add.</param>
+        /// <returns>The added Video object.</returns>
         Task<Video> AddVideo(Video video);
+
+        /// <summary>
+        /// Updates a Video entity in the database and the cache.
+        /// </summary>
+        /// <param name="video">The Video object with the updated properties.</param>
+        /// <returns>The updated Video object.</returns>
         Task<Video> UpdateVideo(Video video);
+
+        /// <summary>
+        /// Deletes a Video entity from the database and the cache.
+        /// </summary>
+        /// <param name="video">The Video object to delete.</param>
+        /// <returns>The deleted Video object.</returns>
         Task<Video> DeleteVideo(Video video);
+
+        /// <summary>
+        /// Removes a Video entity from the cache.
+        /// Also updates the list of all Videos for the Progeny in the cache.
+        /// </summary>
+        /// <param name="videoId">The VideoId of the Video item to remove.</param>
+        /// <param name="progenyId">The ProgenyId of the Progeny that the Video belongs to.</param>
+        /// <returns></returns>
         Task RemoveVideoFromCache(int videoId, int progenyId);
+
+        /// <summary>
+        /// Gets a list of all Videos for a Progeny.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get all Videos for.</param>
+        /// <returns>List of Video objects.</returns>
         Task<List<Video>> GetVideosList(int progenyId);
+
+        /// <summary>
+        /// Gets a list of all Videos for a Progeny from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get and set Videos for.</param>
+        /// <returns>List of Video objects.</returns>
         Task<List<Video>> SetVideosListInCache(int progenyId);
     }
 }
--- KinaUnaProgenyApi/Services/VideosService.cs
+++ KinaUnaProgenyApi/Services/VideosService.cs
@@ -26,6 +26,12 @@ namespace KinaUnaProgenyApi.Services
             _cacheOptionsSliding.SetSlidingExpiration(new System.TimeSpan(96, 0, 0)); // Expire after 24 hours.
         }
 
+        /// <summary>
+        /// Gets a Video entity with the specified VideoId.
+        /// First checks the cache, if not found, gets the Video from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="id">The VideoId of the Video to get.</param>
+        /// <returns>The Video object with the given VideoId. Null if the Video item doesn't exist.</returns>
         public async Task<Video> GetVideo(int id)
         {
             Video video = await GetVideoFromCache(id);
@@ -37,24 +43,40 @@ namespace KinaUnaProgenyApi.Services
             return video;
         }
 
+        /// <summary>
+        /// Gets a Video entity with the specified VideoLink.
+        /// </summary>
+        /// <param name="link">The VideoLink of the Video item to get.</param>
+        /// <param name="progenyId">The ProgenyId of the Video item to get.</param>
+        /// <returns>The Video with the given VideoLink and ProgenyId. Null if the Video doesn't exist.</returns>
         public async Task<Video> GetVideoByLink(string link, int progenyId)
         {
             Video result = await _mediaContext.VideoDb.FirstOrDefaultAsync(v => v.VideoLink == link && v.ProgenyId == progenyId);
             return result;
         }
 
+        /// <summary>
+        /// Gets the Video entity with the specified VideoId from the cache.
+        /// </summary>
+        /// <param name="id">The VideoId of the Video to get.</param>
+        /// <returns>The Video with the given VideoId. Null if the Video isn't in the cache.</returns>
         private async Task<Video> GetVideoFromCache(int id)
         {
-            Video video = new();
             string cachedVideo = await _cache.GetStringAsync(Constants.AppName + Constants.ApiVersion + "video" + id);
-            if (!string.IsNullOrEmpty(cachedVideo))
+            if (string.IsNullOrEmpty(cachedVideo))
             {
-                video = JsonConvert.DeserializeObject<Video>(cachedVideo);
+                return null;
             }
 
+            Video video = JsonConvert.DeserializeObject<Video>(cachedVideo);
             return video;
         }
 
+        /// <summary>
+        /// Gets a Video entity with the specified VideoId from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="id">The VideoId of the Video to get and set.</param>
+        /// <returns>The Video object with the given VideoId. Null if the Video doesn't exist.</returns>
         public async Task<Video> SetVideoInCache(int id)
         {
             Video video = await _mediaContext.VideoDb.AsNoTracking().SingleOrDefaultAsync(v => v.VideoId == id);
@@ -74,6 +96,11 @@ namespace KinaUnaProgenyApi.Services
             return video;
         }
 
+        /// <summary>
+        /// Adds a new Video entity to the database and adds it to the cache.
+        /// </summary>
+        /// <param name="video">The Video object to add.</param>
+        /// <returns>The added Video object.</returns>
         public async Task<Video> AddVideo(Video video)
         {
             video.RemoveNullStrings();
@@ -85,6 +112,11 @@ namespace KinaUnaProgenyApi.Services
             return video;
         }
 
+        /// <summary>
+        /// Updates a Video entity in the database and the cache.
+        /// </summary>
+        /// <param name="video">The Video object with the updated properties.</param>
+        /// <returns>The updated Video object.</returns>
         public async Task<Video> UpdateVideo(Video video)
         {
             video.RemoveNullStrings();
@@ -102,6 +134,11 @@ namespace KinaUnaProgenyApi.Services
             return video;
         }
 
+        /// <summary>
+        /// Deletes a Video entity from the database and the cache.
+        /// </summary>
+        /// <param name="video">The Video object to delete.</param>
+        /// <returns>The deleted Video object.</returns>
         public async Task<Video> DeleteVideo(Video video)
         {
             Video videoToDelete = await _mediaContext.VideoDb.SingleOrDefaultAsync(v => v.VideoId == video.VideoId);
@@ -114,12 +151,24 @@ namespace KinaUnaProgenyApi.Services
             return video;
         }
 
+        /// <summary>
+        /// Removes a Video entity from the cache.
+        /// Also updates the list of all Videos for the Progeny in the cache.
+        /// </summary>
+        /// <param name="videoId">The VideoId of the Video item to remove.</param>
+        /// <param name="progenyId">The ProgenyId of the Progeny that the Video belongs to.</param>
+        /// <returns></returns>
         public async Task RemoveVideoFromCache(int videoId, int progenyId)
         {
             await _cache.RemoveAsync(Constants.AppName + Constants.ApiVersion + "video" + videoId);
             _ = await SetVideosListInCache(progenyId);
         }
 
+        /// <summary>
+        /// Gets a list of all Videos for a Progeny.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get all Videos for.</param>
+        /// <returns>List of Video objects.</returns>
         public async Task<List<Video>> GetVideosList(int progenyId)
         {
             List<Video> videosList = await GetVideosListFromCache(progenyId);
@@ -131,6 +180,11 @@ namespace KinaUnaProgenyApi.Services
             return videosList;
         }
 
+        /// <summary>
+        /// Gets a list of all Videos for a Progeny from the cache.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get Videos for.</param>
+        /// <returns>List of Video objects.</returns>
         private async Task<List<Video>> GetVideosListFromCache(int progenyId)
         {
             List<Video> videosList = [];
@@ -143,6 +197,11 @@ namespace KinaUnaProgenyApi.Services
             return videosList;
         }
 
+        /// <summary>
+        /// Gets a list of all Videos for a Progeny from the database and adds it to the cache.
+        /// </summary>
+        /// <param name="progenyId">The ProgenyId of the Progeny to get and set Videos for.</param>
+        /// <returns>List of Video objects.</returns>
         public async Task<List<Video>> SetVideosListInCache(int progenyId)
         {
             List<Video> videosList = await _mediaContext.VideoDb.AsNoTracking().Where(v => v.ProgenyId == progenyId).ToListAsync();
