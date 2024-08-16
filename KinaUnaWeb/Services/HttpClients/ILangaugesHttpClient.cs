96ef31a19679d3beb5f2891b455a6b223f8fc7e3
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-16 11:53:40 +0200

    Documentation update. Added summary comments for methods in Web/Services/HttpClients/ILanguagesHttpClient.cs and Web/Services/HttpClients/LanguagesHttpClient.cs

--- KinaUnaWeb/Services/HttpClients/ILangaugesHttpClient.cs
+++ KinaUnaWeb/Services/HttpClients/ILangaugesHttpClient.cs
@@ -4,13 +4,50 @@ using KinaUna.Data.Models;
 
 namespace KinaUnaWeb.Services.HttpClients
 {
+    /// <summary>
+    /// Provides methods for interacting with the Languages API.
+    /// </summary>
     public interface ILanguagesHttpClient
     {
+        /// <summary>
+        /// Gets the list of all KinaUnaLanguages from the cache.
+        /// If the list is not found in the cache, or if updateCache is true, the list is fetched from the API.
+        /// </summary>
+        /// <param name="updateCache">Get the list from the API first and update the cache.</param>
+        /// <returns>List of KinaUnaLanguage objects.</returns>
         Task<List<KinaUnaLanguage>> GetAllLanguages(bool updateCache = false);
 
+        /// <summary>
+        /// Gets the KinaUnaLanguage with the given Id.
+        /// </summary>
+        /// <param name="languageId">The Id of the KinaUnaLanguage to get.</param>
+        /// <param name="updateCache">Get the KinaUnaLanguage from the API first and update the cache.</param>
+        /// <returns>KinaUnaLanguage object with the given Id.</returns>
         Task<KinaUnaLanguage> GetLanguage(int languageId, bool updateCache = false);
+
+        /// <summary>
+        /// Adds a new KinaUnaLanguage.
+        /// Only KinaUnaAdmins can add new languages.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object to add.</param>
+        /// <returns>The added KinaUnaLanguage object.</returns>
         Task<KinaUnaLanguage> AddLanguage(KinaUnaLanguage language);
+
+        /// <summary>
+        /// Updates a KinaUnaLanguage.
+        /// Only KinaUnaAdmins can update languages.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object with the updated properties.</param>
+        /// <returns>The updated KinaUnaLanguage object.</returns>
         Task<KinaUnaLanguage> UpdateLanguage(KinaUnaLanguage language);
+
+        /// <summary>
+        /// Deletes a KinaUnaLanguage and removes it from the cache.
+        /// Also removes the list of all languages from the cache, forcing a cache update.
+        /// Only KinaUnaAdmins can delete languages.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object to delete.</param>
+        /// <returns>The deleted KinaUnaLanguage object.</returns>
         Task<KinaUnaLanguage> DeleteLanguage(KinaUnaLanguage language);
     }
 }
--- KinaUnaWeb/Services/HttpClients/LanguagesHttpClient.cs
+++ KinaUnaWeb/Services/HttpClients/LanguagesHttpClient.cs
@@ -11,6 +11,9 @@ using Newtonsoft.Json;
 
 namespace KinaUnaWeb.Services.HttpClients
 {
+    /// <summary>
+    /// Provides methods for interacting with the Languages API.
+    /// </summary>
     public class LanguagesHttpClient : ILanguagesHttpClient
     {
         private readonly HttpClient _httpClient;
@@ -31,6 +34,12 @@ namespace KinaUnaWeb.Services.HttpClients
             httpClient.DefaultRequestVersion = new Version(2, 0);
         }
 
+        /// <summary>
+        /// Gets the list of all KinaUnaLanguages from the cache.
+        /// If the list is not found in the cache, or if updateCache is true, the list is fetched from the API.
+        /// </summary>
+        /// <param name="updateCache">Get the list from the API first and update the cache.</param>
+        /// <returns>List of KinaUnaLanguage objects.</returns>
         public async Task<List<KinaUnaLanguage>> GetAllLanguages(bool updateCache = false)
         {
             List<KinaUnaLanguage> languageList = [];
@@ -59,6 +68,12 @@ namespace KinaUnaWeb.Services.HttpClients
             return languageList;
         }
 
+        /// <summary>
+        /// Gets the KinaUnaLanguage with the given Id.
+        /// </summary>
+        /// <param name="languageId">The Id of the KinaUnaLanguage to get.</param>
+        /// <param name="updateCache">Get the KinaUnaLanguage from the API first and update the cache.</param>
+        /// <returns>KinaUnaLanguage object with the given Id.</returns>
         public async Task<KinaUnaLanguage> GetLanguage(int languageId, bool updateCache = false)
         {
             KinaUnaLanguage language = new();
@@ -84,6 +99,12 @@ namespace KinaUnaWeb.Services.HttpClients
             return language;
         }
 
+        /// <summary>
+        /// Adds a new KinaUnaLanguage.
+        /// Only KinaUnaAdmins can add new languages.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object to add.</param>
+        /// <returns>The added KinaUnaLanguage object.</returns>
         public async Task<KinaUnaLanguage> AddLanguage(KinaUnaLanguage language)
         {
             KinaUnaLanguage addedLanguage = new();
@@ -100,7 +121,12 @@ namespace KinaUnaWeb.Services.HttpClients
             return addedLanguage;
         }
 
-
+        /// <summary>
+        /// Updates a KinaUnaLanguage.
+        /// Only KinaUnaAdmins can update languages.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object with the updated properties.</param>
+        /// <returns>The updated KinaUnaLanguage object.</returns>
         public async Task<KinaUnaLanguage> UpdateLanguage(KinaUnaLanguage language)
         {
             KinaUnaLanguage updatedLanguage = new();
@@ -117,6 +143,13 @@ namespace KinaUnaWeb.Services.HttpClients
             return updatedLanguage;
         }
 
+        /// <summary>
+        /// Deletes a KinaUnaLanguage and removes it from the cache.
+        /// Also removes the list of all languages from the cache, forcing a cache update.
+        /// Only KinaUnaAdmins can delete languages.
+        /// </summary>
+        /// <param name="language">The KinaUnaLanguage object to delete.</param>
+        /// <returns>The deleted KinaUnaLanguage object.</returns>
         public async Task<KinaUnaLanguage> DeleteLanguage(KinaUnaLanguage language)
         {
             KinaUnaLanguage deletedLanguage = new();
