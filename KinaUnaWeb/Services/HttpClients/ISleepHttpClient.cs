4abf375518d691a77ad837b641034b8de5c8fe90
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-16 20:20:43 +0200

    Documentation update. Added summary comments for methods in Web/Services/HttpClients/ISleepHttpClient.cs and Web/Services/HttpClients/SleepHttpClient.cs

--- KinaUnaWeb/Services/HttpClients/ISleepHttpClient.cs
+++ KinaUnaWeb/Services/HttpClients/ISleepHttpClient.cs
@@ -4,41 +4,44 @@ using KinaUna.Data.Models;
 
 namespace KinaUnaWeb.Services.HttpClients
 {
+    /// <summary>
+    /// Provides methods to interact with the Sleep API.
+    /// </summary>
     public interface ISleepHttpClient
     {
         /// <summary>
         /// Gets a Sleep with a given SleepId.
         /// </summary>
-        /// <param name="sleepId">int: The Id of the sleep object (Sleep.SleepId).</param>
-        /// <returns>Sleep</returns>
+        /// <param name="sleepId">The SleepId of the Sleep to get.</param>
+        /// <returns>The Sleep object with the given SleepId. If the item cannot be found a new Sleep object with SleepId=0 is returned.</returns>
         Task<Sleep> GetSleepItem(int sleepId);
 
         /// <summary>
         /// Adds a new Sleep object.
         /// </summary>
-        /// <param name="sleep">Sleep: The Sleep object to be added.</param>
-        /// <returns>Sleep: The added sleep object.</returns>
+        /// <param name="sleep">The Sleep object to be added.</param>
+        /// <returns>The added Sleep object.</returns>
         Task<Sleep> AddSleep(Sleep sleep);
 
         /// <summary>
         /// Updates a Sleep object. The Sleep with the same SleepId will be updated.
         /// </summary>
-        /// <param name="sleep">Sleep: The Sleep object to update.</param>
-        /// <returns>Sleep: The updated Sleep object.</returns>
+        /// <param name="sleep">The Sleep object with the updated properties.</param>
+        /// <returns>The updated Sleep object.</returns>
         Task<Sleep> UpdateSleep(Sleep sleep);
 
         /// <summary>
-        /// Removes the Sleep object with a given SleepId.
+        /// Deletes the Sleep object with a given SleepId.
         /// </summary>
-        /// <param name="sleepId">int: The id of the Sleep object to remove (Sleep.SleepId).</param>
-        /// <returns>bool: True if the Sleep object was successfully removed.</returns>
+        /// <param name="sleepId">The SleepId of the Sleep object to delete.</param>
+        /// <returns>bool: True if the Sleep object was successfully deleted.</returns>
         Task<bool> DeleteSleepItem(int sleepId);
 
         /// <summary>
-        /// Gets the List of Sleep objects for a progeny that a user has access to.
+        /// Gets the List of all Sleep objects for a Progeny that a user has access to.
         /// </summary>
-        /// <param name="progenyId">int: The id of the progeny (Progeny.Id).</param>
-        /// <param name="accessLevel">int: The access level of the user.</param>
+        /// <param name="progenyId">The Id of the Progeny.</param>
+        /// <param name="accessLevel">The access level of the user for the Progeny.</param>
         /// <returns>List of Sleep objects.</returns>
         Task<List<Sleep>> GetSleepList(int progenyId, int accessLevel);
     }
--- KinaUnaWeb/Services/HttpClients/SleepHttpClient.cs
+++ KinaUnaWeb/Services/HttpClients/SleepHttpClient.cs
@@ -10,6 +10,9 @@ using Newtonsoft.Json;
 
 namespace KinaUnaWeb.Services.HttpClients
 {
+    /// <summary>
+    /// Provides methods to interact with the Sleep API.
+    /// </summary>
     public class SleepHttpClient : ISleepHttpClient
     {
         private readonly HttpClient _httpClient;
@@ -27,6 +30,11 @@ namespace KinaUnaWeb.Services.HttpClients
             httpClient.DefaultRequestVersion = new Version(2, 0);
         }
 
+        /// <summary>
+        /// Gets a Sleep with a given SleepId.
+        /// </summary>
+        /// <param name="sleepId">The SleepId of the Sleep to get.</param>
+        /// <returns>The Sleep object with the given SleepId. If the item cannot be found a new Sleep object with SleepId=0 is returned.</returns>
         public async Task<Sleep> GetSleepItem(int sleepId)
         {
             string accessToken = await _apiTokenClient.GetProgenyAndMediaApiToken();
@@ -41,6 +49,11 @@ namespace KinaUnaWeb.Services.HttpClients
             return sleepItem ?? new Sleep();
         }
 
+        /// <summary>
+        /// Adds a new Sleep object.
+        /// </summary>
+        /// <param name="sleep">The Sleep object to be added.</param>
+        /// <returns>The added Sleep object.</returns>
         public async Task<Sleep> AddSleep(Sleep sleep)
         {
             string accessToken = await _apiTokenClient.GetProgenyAndMediaApiToken();
@@ -55,6 +68,11 @@ namespace KinaUnaWeb.Services.HttpClients
             return sleep ?? new Sleep();
         }
 
+        /// <summary>
+        /// Updates a Sleep object. The Sleep with the same SleepId will be updated.
+        /// </summary>
+        /// <param name="sleep">The Sleep object with the updated properties.</param>
+        /// <returns>The updated Sleep object.</returns>
         public async Task<Sleep> UpdateSleep(Sleep sleep)
         {
             string accessToken = await _apiTokenClient.GetProgenyAndMediaApiToken();
@@ -69,6 +87,11 @@ namespace KinaUnaWeb.Services.HttpClients
             return sleep ?? new Sleep();
         }
 
+        /// <summary>
+        /// Deletes the Sleep object with a given SleepId.
+        /// </summary>
+        /// <param name="sleepId">The SleepId of the Sleep object to delete.</param>
+        /// <returns>bool: True if the Sleep object was successfully deleted.</returns>
         public async Task<bool> DeleteSleepItem(int sleepId)
         {
             string accessToken = await _apiTokenClient.GetProgenyAndMediaApiToken();
@@ -79,6 +102,12 @@ namespace KinaUnaWeb.Services.HttpClients
             return deleteSleepResponse.IsSuccessStatusCode;
         }
 
+        /// <summary>
+        /// Gets the List of all Sleep objects for a Progeny that a user has access to.
+        /// </summary>
+        /// <param name="progenyId">The Id of the Progeny.</param>
+        /// <param name="accessLevel">The access level of the user for the Progeny.</param>
+        /// <returns>List of Sleep objects.</returns>
         public async Task<List<Sleep>> GetSleepList(int progenyId, int accessLevel)
         {
             List<Sleep> progenySleepList = [];
