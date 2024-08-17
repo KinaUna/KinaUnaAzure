638731650fb217b836c6a5689f4d0c98bc993da8
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-17 16:57:14 +0200

    Documentation update. Added summary comments for methods in Web/Services/ILocaleManager.cs and Web/Services/LocaleManager.cs

--- KinaUnaWeb/Services/ILocaleManager.cs
+++ KinaUnaWeb/Services/ILocaleManager.cs
@@ -5,12 +5,43 @@ using Microsoft.AspNetCore.Http;
 
 namespace KinaUnaWeb.Services
 {
+    /// <summary>
+    /// Service for managing languages, translations and page texts.
+    /// </summary>
     public interface ILocaleManager
     {
+        /// <summary>
+        /// Generates a SetLanguageIdViewModel object with a list of available languages and the current language id.
+        /// </summary>
+        /// <param name="currentLanguageId">The Id of the currently selected language.</param>
+        /// <returns>SetLanguageIdViewModel object.</returns>
         Task<SetLanguageIdViewModel> GetLanguageModel(int currentLanguageId);
-        
+
+        /// <summary>
+        /// Gets a translation for a given word (or phrase) on a given page, in a given language.
+        /// </summary>
+        /// <param name="word">The word (or phrase) to get a translation for.</param>
+        /// <param name="page">The page the word appears on.</param>
+        /// <param name="languageId">The Id of the language to translate the word into.</param>
+        /// <returns>String with the translated word.</returns>
         Task<string> GetTranslation(string word, string page, int languageId);
+
+        /// <summary>
+        /// Gets a KinaUnaText object for a given title on a given page, in a given language.
+        /// </summary>
+        /// <param name="title">The Title of the KinaUnaText.</param>
+        /// <param name="page">The page the text appears on.</param>
+        /// <param name="languageId">The Id of the language it should be translated into.</param>
+        /// <returns>KinaUnaText object.</returns>
         Task<KinaUnaText> GetPageTextByTitle(string title, string page, int languageId);
+
+        /// <summary>
+        /// Gets the current user's language Id.
+        /// First tries to get the language Id from the cookie, if not found, it tries to get the language from the browser settings.
+        /// If no language is found, it defaults to language id 1 = English.
+        /// </summary>
+        /// <param name="request">The current HttpRequest.</param>
+        /// <returns>Integer for the language Id.</returns>
         int GetLanguageId(HttpRequest request);
     }
 }
--- KinaUnaWeb/Services/LocaleManager.cs
+++ KinaUnaWeb/Services/LocaleManager.cs
@@ -7,9 +7,20 @@ using Microsoft.AspNetCore.Http;
 
 namespace KinaUnaWeb.Services
 {
+    /// <summary>
+    /// Service for managing languages, translations and page texts.
+    /// </summary>
+    /// <param name="languagesHttpClient"></param>
+    /// <param name="translationsHttpClient"></param>
+    /// <param name="pageTextsHttpClient"></param>
     public class LocaleManager(ILanguagesHttpClient languagesHttpClient, ITranslationsHttpClient translationsHttpClient, IPageTextsHttpClient pageTextsHttpClient)
         : ILocaleManager
     {
+        /// <summary>
+        /// Generates a SetLanguageIdViewModel object with a list of available languages and the current language id.
+        /// </summary>
+        /// <param name="currentLanguageId">The Id of the currently selected language.</param>
+        /// <returns>SetLanguageIdViewModel object.</returns>
         public async Task<SetLanguageIdViewModel> GetLanguageModel(int currentLanguageId)
         {
             SetLanguageIdViewModel languageIdModel = new()
@@ -21,19 +32,40 @@ namespace KinaUnaWeb.Services
             return languageIdModel;
 
         }
-        
+
+        /// <summary>
+        /// Gets a translation for a given word (or phrase) on a given page, in a given language.
+        /// </summary>
+        /// <param name="word">The word (or phrase) to get a translation for.</param>
+        /// <param name="page">The page the word appears on.</param>
+        /// <param name="languageId">The Id of the language to translate the word into.</param>
+        /// <returns>String with the translated word.</returns>
         public async Task<string> GetTranslation(string word, string page, int languageId)
         {
             string translation = await translationsHttpClient.GetTranslation(word, page, languageId);
             return translation;
         }
 
+        /// <summary>
+        /// Gets a KinaUnaText object for a given title on a given page, in a given language.
+        /// </summary>
+        /// <param name="title">The Title of the KinaUnaText.</param>
+        /// <param name="page">The page the text appears on.</param>
+        /// <param name="languageId">The Id of the language it should be translated into.</param>
+        /// <returns>KinaUnaText object.</returns>
         public async Task<KinaUnaText> GetPageTextByTitle(string title, string page, int languageId)
         {
             KinaUnaText text = await pageTextsHttpClient.GetPageTextByTitle(title, page, languageId);
             return text;
         }
 
+        /// <summary>
+        /// Gets the current user's language Id.
+        /// First tries to get the language Id from the cookie, if not found, it tries to get the language from the browser settings.
+        /// If no language is found, it defaults to language id 1 = English.
+        /// </summary>
+        /// <param name="request">The current HttpRequest.</param>
+        /// <returns>Integer for the language Id.</returns>
         public int GetLanguageId(HttpRequest request)
         {
             return request.GetLanguageIdFromCookie();
