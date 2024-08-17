084df366911f9c4313a763512b795fb6b404711d
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-17 20:24:33 +0200

    Documentation update. Added summary comments for methods in Web/Services/IWebNotificationsService.cs and Web/Services/WebNotificationsService.cs

--- KinaUnaWeb/Services/IWebNotificationsService.cs
+++ KinaUnaWeb/Services/IWebNotificationsService.cs
@@ -4,15 +4,61 @@ using System.Threading.Tasks;
 
 namespace KinaUnaWeb.Services
 {
+    /// <summary>
+    /// Service for managing WebNotifications.
+    /// </summary>
     public interface IWebNotificationsService
     {
+        /// <summary>
+        /// Saves a WebNotification to the database.
+        /// </summary>
+        /// <param name="notification">The WebNotification to save.</param>
+        /// <returns>The saved WebNotification object.</returns>
         Task<WebNotification> SaveNotification(WebNotification notification);
+
+        /// <summary>
+        /// Updates a WebNotification in the database.
+        /// </summary>
+        /// <param name="notification">The WebNotification with the updated properties.</param>
+        /// <returns>The updated WebNotification.</returns>
         Task<WebNotification> UpdateNotification(WebNotification notification);
+
+        /// <summary>
+        /// Removes a WebNotification from the database.
+        /// </summary>
+        /// <param name="notification">The WebNotification to remove.</param>
+        /// <returns></returns>
         Task RemoveNotification(WebNotification notification);
+
+        /// <summary>
+        /// Gets a WebNotification by Id.
+        /// </summary>
+        /// <param name="id">The Id of the WebNotification to get.</param>
+        /// <returns>The WebNotification with the given Id. If the item cannot be found a new WebNotification with Id=0 is returned.</returns>
         Task<WebNotification> GetNotificationById(int id);
 
+        /// <summary>
+        /// Gets a list of all WebNotifications for a user.
+        /// </summary>
+        /// <param name="userId">The UserId of the user to get WebNotifications for.</param>
+        /// <returns>List of WebNotification objects.</returns>
         Task<List<WebNotification>> GetUsersNotifications(string userId);
+
+        /// <summary>
+        /// Gets a list of the latest WebNotifications for a user.
+        /// </summary>
+        /// <param name="userId">The user's UserId.</param>
+        /// <param name="start">Number of WebNotifications to skip.</param>
+        /// <param name="count">Number of WebNotifications to include.</param>
+        /// <param name="unreadOnly">Include only unread WebNotifications.</param>
+        /// <returns>List of WebNotification objects.</returns>
         Task<List<WebNotification>> GetLatestNotifications(string userId, int start = 0, int count = 10, bool unreadOnly = true);
+
+        /// <summary>
+        /// Gets the number of WebNotifications for a user, including both read and unread ones.
+        /// </summary>
+        /// <param name="userId">The UserId of the user to get the count for.</param>
+        /// <returns>Integer with the count of WebNotifications.</returns>
         Task<int> GetUsersNotificationsCount(string userId);
     }
 }
--- KinaUnaWeb/Services/WebNotificationsService.cs
+++ KinaUnaWeb/Services/WebNotificationsService.cs
@@ -5,8 +5,17 @@ using KinaUnaWeb.Services.HttpClients;
 
 namespace KinaUnaWeb.Services
 {
+    /// <summary>
+    /// Service for managing WebNotifications.
+    /// </summary>
+    /// <param name="webNotificationsHttpClient"></param>
     public class WebNotificationsService(IWebNotificationsHttpClient webNotificationsHttpClient) : IWebNotificationsService
     {
+        /// <summary>
+        /// Saves a WebNotification to the database.
+        /// </summary>
+        /// <param name="notification">The WebNotification to save.</param>
+        /// <returns>The saved WebNotification object.</returns>
         public async Task<WebNotification> SaveNotification(WebNotification notification)
         {
             notification = await webNotificationsHttpClient.AddWebNotification(notification);
@@ -14,6 +23,11 @@ namespace KinaUnaWeb.Services
             return notification;
         }
 
+        /// <summary>
+        /// Updates a WebNotification in the database.
+        /// </summary>
+        /// <param name="notification">The WebNotification with the updated properties.</param>
+        /// <returns>The updated WebNotification.</returns>
         public async Task<WebNotification> UpdateNotification(WebNotification notification)
         {
             notification = await webNotificationsHttpClient.UpdateWebNotification(notification);
@@ -21,11 +35,21 @@ namespace KinaUnaWeb.Services
             return notification;
         }
 
+        /// <summary>
+        /// Removes a WebNotification from the database.
+        /// </summary>
+        /// <param name="notification">The WebNotification to remove.</param>
+        /// <returns></returns>
         public async Task RemoveNotification(WebNotification notification)
         {
             await webNotificationsHttpClient.RemoveWebNotification(notification);
         }
 
+        /// <summary>
+        /// Gets a WebNotification by Id.
+        /// </summary>
+        /// <param name="id">The Id of the WebNotification to get.</param>
+        /// <returns>The WebNotification with the given Id. If the item cannot be found a new WebNotification with Id=0 is returned.</returns>
         public async Task<WebNotification> GetNotificationById(int id)
         {
             WebNotification notification = await webNotificationsHttpClient.GetWebNotificationById(id);
@@ -33,6 +57,11 @@ namespace KinaUnaWeb.Services
             return notification;
         }
 
+        /// <summary>
+        /// Gets a list of all WebNotifications for a user.
+        /// </summary>
+        /// <param name="userId">The UserId of the user to get WebNotifications for.</param>
+        /// <returns>List of WebNotification objects.</returns>
         public async Task<List<WebNotification>> GetUsersNotifications(string userId)
         {
             List<WebNotification> usersNotifications = await webNotificationsHttpClient.GetUsersWebNotifications(userId);
@@ -40,6 +69,14 @@ namespace KinaUnaWeb.Services
             return usersNotifications;
         }
 
+        /// <summary>
+        /// Gets a list of the latest WebNotifications for a user.
+        /// </summary>
+        /// <param name="userId">The user's UserId.</param>
+        /// <param name="start">Number of WebNotifications to skip.</param>
+        /// <param name="count">Number of WebNotifications to include.</param>
+        /// <param name="unreadOnly">Include only unread WebNotifications.</param>
+        /// <returns>List of WebNotification objects.</returns>
         public async Task<List<WebNotification>> GetLatestNotifications(string userId, int start = 0, int count = 10, bool unreadOnly = true)
         {
             List<WebNotification> latestNotifications = await webNotificationsHttpClient.GetLatestWebNotifications(userId, start, count, unreadOnly);
@@ -47,6 +84,11 @@ namespace KinaUnaWeb.Services
             return latestNotifications;
         }
 
+        /// <summary>
+        /// Gets the number of WebNotifications for a user, including both read and unread ones.
+        /// </summary>
+        /// <param name="userId">The UserId of the user to get the count for.</param>
+        /// <returns>Integer with the count of WebNotifications.</returns>
         public async Task<int> GetUsersNotificationsCount(string userId)
         {
             int notificationsCount = await webNotificationsHttpClient.GetUsersNotificationsCount(userId);
