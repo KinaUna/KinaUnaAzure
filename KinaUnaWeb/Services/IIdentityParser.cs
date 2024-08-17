c99b837a1b76c87e8a2e82a2e6f208f96d0861f3
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-17 16:38:30 +0200

    Documentation update. Added summary comments for methods in Web/Services/ImageStore.cs.

--- KinaUnaWeb/Services/IIdentityParser.cs
+++ KinaUnaWeb/Services/IIdentityParser.cs
@@ -2,8 +2,16 @@
 
 namespace KinaUnaWeb.Services
 {
+    /// <summary>
+    /// Parsing methods for converting ClaimsPrincipal (usually HttpContext.User) to other types.
+    /// </summary>
     public interface IIdentityParser<T>
     {
+        /// <summary>
+        /// Parses a ClaimsPrincipal (usually HttpContext.User) to an ApplicationUser object.
+        /// </summary>
+        /// <param name="principal">The ClaimsPrincipal (i.e. User) to parse.</param>
+        /// <returns>ApplicationUser object.</returns>
         T Parse(IPrincipal principal);
     }
 }
--- KinaUnaWeb/Services/ImageStore.cs
+++ KinaUnaWeb/Services/ImageStore.cs
@@ -11,6 +11,10 @@ using KinaUna.Data.Models;
 
 namespace KinaUnaWeb.Services
 {
+    /// <summary>
+    /// Provides methods for saving, retrieving and deleting images from Azure Blob Storage.
+    /// </summary>
+    /// <param name="configuration"></param>
     public class ImageStore(IConfiguration configuration)
     {
         private readonly BlobServiceClient _blobServiceClient = new(configuration.GetValue<string>("BlobStorageConnectionString"));
@@ -18,6 +22,13 @@ namespace KinaUnaWeb.Services
         private readonly string _baseUri = configuration.GetValue<string>("CloudBlobBase");
         private readonly string _cloudBlobUserName = configuration.GetValue<string>("CloudBlobUserName");
 
+        /// <summary>
+        /// Saves a stream of image data to Azure Blob Storage.
+        /// </summary>
+        /// <param name="imageStream">The image data stream to save.</param>
+        /// <param name="containerName">The name of the container to store the image in.</param>
+        /// <param name="fileFormat">The file extension to save the file with.</param>
+        /// <returns>The filename of the saved file.</returns>
         public async Task<string> SaveImage(Stream imageStream, string containerName = "pictures", string fileFormat = ".jpg")
         {
             string imageId = Guid.NewGuid() + fileFormat;
@@ -29,6 +40,13 @@ namespace KinaUnaWeb.Services
             return imageId;
         }
 
+        /// <summary>
+        /// Gets a URI for an image in Azure Blob Storage.
+        /// SAS tokens expire after a limited time, so the URI is only valid for a short time.
+        /// </summary>
+        /// <param name="imageId">The file name of the image to get a URL for.</param>
+        /// <param name="containerName">The name of the container the image file is stored in.</param>
+        /// <returns>String with the URL for the image.</returns>
         public string UriFor(string imageId, string containerName = "pictures")
         {
             if (imageId.StartsWith("http", StringComparison.CurrentCultureIgnoreCase))
@@ -59,6 +77,12 @@ namespace KinaUnaWeb.Services
             return sasUri.Uri.AbsoluteUri;
         }
 
+        /// <summary>
+        /// Gets a stream of image data from Azure Blob Storage.
+        /// </summary>
+        /// <param name="imageId">The file name of the image.</param>
+        /// <param name="containerName">The name of the container the image file is stored in.</param>
+        /// <returns>MemoryStream with the image data. If the image file isn't found a default dummy file is used.</returns>
         public async Task<MemoryStream> GetStream(string imageId, string containerName = "pictures")
         {
             try
@@ -83,6 +107,12 @@ namespace KinaUnaWeb.Services
             }
         }
 
+        /// <summary>
+        /// Deletes an image from Azure Blob Storage.
+        /// </summary>
+        /// <param name="imageId">The file name of the image.</param>
+        /// <param name="containerName">The container the image file is stored in.</param>
+        /// <returns>The filename of the deleted file. If the imageId isn't a file name nothing is deleted and the default ProfilePictureUrl is returned instead.</returns>
         public async Task<string> DeleteImage(string imageId, string containerName = "pictures")
         {
             if (string.IsNullOrEmpty(imageId) || imageId.StartsWith("http", StringComparison.CurrentCultureIgnoreCase))
