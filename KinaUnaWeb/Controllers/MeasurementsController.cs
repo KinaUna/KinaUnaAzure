6061199892d0013eaa825b75fb1519fa3293ebbb
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-22 20:00:57 +0200

    Documentation update. Added summary comments for actions in Web/Controllers/MeasurementsController.cs

--- KinaUnaWeb/Controllers/MeasurementsController.cs
+++ KinaUnaWeb/Controllers/MeasurementsController.cs
@@ -14,6 +14,11 @@ namespace KinaUnaWeb.Controllers
 {
     public class MeasurementsController(IMeasurementsHttpClient measurementsHttpClient, IViewModelSetupService viewModelSetupService) : Controller
     {
+        /// <summary>
+        /// Measurement Index page. Shows a list of all measurements for a Progeny.
+        /// </summary>
+        /// <param name="childId">The Id of the Progeny to show Measurements for.</param>
+        /// <returns>View with MeasurementViewModel.</returns>
         [AllowAnonymous]
         public async Task<IActionResult> Index(int childId = 0)
         {
@@ -40,6 +45,12 @@ namespace KinaUnaWeb.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// Page or Partial view to show a single Measurement.
+        /// </summary>
+        /// <param name="measurementId">The MeasurementId of the Measurement to view.</param>
+        /// <param name="partialView">Return partial view, for fetching HTML inline to show in a modal/popup.</param>
+        /// <returns>View or PartialView with MeasurementViewModel.</returns>
         [AllowAnonymous]
         public async Task<IActionResult> ViewMeasurement(int measurementId, bool partialView = false)
         {
@@ -67,6 +78,10 @@ namespace KinaUnaWeb.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// Page to add a new Measurement.
+        /// </summary>
+        /// <returns>View with MeasurementViewModel.</returns>
         [HttpGet]
         public async Task<IActionResult> AddMeasurement()
         {
@@ -86,6 +101,11 @@ namespace KinaUnaWeb.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// HttpPost endpoint for adding a new measurement.
+        /// </summary>
+        /// <param name="model">MeasurementViewModel with the properties of the Measurement to add.</param>
+        /// <returns>Redirects to Measurements/Index page.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddMeasurement(MeasurementViewModel model)
@@ -107,6 +127,11 @@ namespace KinaUnaWeb.Controllers
             return RedirectToAction("Index", "Measurements");
         }
 
+        /// <summary>
+        /// Page to edit a Measurement.
+        /// </summary>
+        /// <param name="itemId">The MeasurementId of the Measurement to edit.</param>
+        /// <returns>View with MeasurementViewModel.</returns>
         [HttpGet]
         public async Task<IActionResult> EditMeasurement(int itemId)
         {
@@ -127,6 +152,11 @@ namespace KinaUnaWeb.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// HttpPost endpoint for editing a Measurement.
+        /// </summary>
+        /// <param name="model">MeasurementViewModel with the updated properties of the Measurement that was edited.</param>
+        /// <returns>Redirects to Measurements/Index page.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditMeasurement(MeasurementViewModel model)
@@ -147,6 +177,11 @@ namespace KinaUnaWeb.Controllers
             return RedirectToAction("Index", "Measurements");
         }
 
+        /// <summary>
+        /// Page to delete a Measurement.
+        /// </summary>
+        /// <param name="itemId">The MeasurementId of the Measurement to delete.</param>
+        /// <returns>View with MeasurementViewModel.</returns>
         [HttpGet]
         public async Task<IActionResult> DeleteMeasurement(int itemId)
         {
@@ -164,6 +199,11 @@ namespace KinaUnaWeb.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// HttpPost endpoint for deleting a Measurement.
+        /// </summary>
+        /// <param name="model">MeasurementViewModel with the properties for the Measurement to delete.</param>
+        /// <returns>Redirects to Measurements/Index page.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteMeasurement(MeasurementViewModel model)
