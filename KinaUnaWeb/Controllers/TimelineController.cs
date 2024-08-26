75e2c3ec34356bbf4dae03880d09e40b431ae0ad
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-26 16:03:48 +0200

    Documentation update. Added summary comments for actions in Web/Controllers/TimelineController.cs

--- KinaUnaWeb/Controllers/TimelineController.cs
+++ KinaUnaWeb/Controllers/TimelineController.cs
@@ -16,6 +16,17 @@ namespace KinaUnaWeb.Controllers
     public class TimelineController(ITimelineHttpClient timelineHttpClient, ITimeLineItemsService timeLineItemsService, IViewModelSetupService viewModelSetupService, IProgenyHttpClient progenyHttpClient)
         : Controller
     {
+        /// <summary>
+        /// Page for showing the Timeline.
+        /// </summary>
+        /// <param name="childId">The Id of the Progeny to show the timeline for.</param>
+        /// <param name="sortBy">Sort order. 0 = oldest first, 1 = newest first.</param>
+        /// <param name="items">Number of TimeLineItems to get.</param>
+        /// <param name="skip">Number of TimeLineItems to skip.</param>
+        /// <param name="year">Start year.</param>
+        /// <param name="month">Start month.</param>
+        /// <param name="day">Start day.</param>
+        /// <returns>View with TimeLineViewModel.</returns>
         [AllowAnonymous]
         public async Task<IActionResult> Index(int childId = 0, int sortBy = 1, int items = 10, int skip = 0, int year=0, int month=0, int day=0)
         {
@@ -54,6 +65,11 @@ namespace KinaUnaWeb.Controllers
             return View(model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching a list of TimeLineItems.
+        /// </summary>
+        /// <param name="parameters">TimeLineParameters object.</param>
+        /// <returns>Json of TimelineList object.</returns>
         [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> GetTimelineList([FromBody] TimelineParameters parameters)
@@ -98,6 +114,11 @@ namespace KinaUnaWeb.Controllers
 
         }
 
+        /// <summary>
+        /// HttpPost method for fetching a list of TimeLineItems from this day in previous years.
+        /// </summary>
+        /// <param name="parameters">TimeLineParameters object.</param>
+        /// <returns>Json of TimelineList object.</returns>
         [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> GetYearAgoList([FromBody] TimelineParameters parameters)
@@ -114,6 +135,11 @@ namespace KinaUnaWeb.Controllers
 
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Picture.
+        /// </summary>
+        /// <param name="model">PictureViewModel with the Picture properties.</param>
+        /// <returns>PartialView with the PictureViewModel.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -122,6 +148,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLinePhotoPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Video.
+        /// </summary>
+        /// <param name="model">VideoViewModel with the Video properties.</param>
+        /// <returns>PartialView with the VideoViewModel.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -130,6 +161,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineVideoPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a CalendarItem.
+        /// </summary>
+        /// <param name="model">CalendarItem with the event properties.</param>
+        /// <returns>PartialView with the CalendarItem.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -138,6 +174,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineEventPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a VocabularyItem.
+        /// </summary>
+        /// <param name="model">VocabularyItem.</param>
+        /// <returns>PartialView with the VocabularyItem.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -146,6 +187,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineVocabularyPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Skill.
+        /// </summary>
+        /// <param name="model">Skill object with the Skill's properties.</param>
+        /// <returns>PartialView with the Skill.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -154,6 +200,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineSkillPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Friend.
+        /// </summary>
+        /// <param name="model">Friend object with the Friend's properties.</param>
+        /// <returns>PartialView with the Friend.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -162,6 +213,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineFriendPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Measurement.
+        /// </summary>
+        /// <param name="model">Measurement object with the Measurement's properties.</param>
+        /// <returns>PartialView with the Measurement.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -170,6 +226,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineMeasurementPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Sleep item.
+        /// </summary>
+        /// <param name="model">Sleep object with the Sleep item's properties.</param>
+        /// <returns>PartialView with the Sleep object.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -178,6 +239,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineSleepPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Note.
+        /// </summary>
+        /// <param name="model">Note object with the Note's properties.</param>
+        /// <returns>PartialView with the Note.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -186,6 +252,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineNotePartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Contact.
+        /// </summary>
+        /// <param name="model">Contact object with the Contact's properties.</param>
+        /// <returns>PartialView with the Contact.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -194,6 +265,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineContactPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Vaccination.
+        /// </summary>
+        /// <param name="model">Vaccination object with the Vaccination's properties.</param>
+        /// <returns>PartialView with the Vaccination.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -202,6 +278,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineVaccinationPartial", model);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem with a Location.
+        /// </summary>
+        /// <param name="model">Location object with the Location's properties.</param>
+        /// <returns>PartialView with the Location.</returns>
         [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -210,6 +291,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView("_TimeLineLocationPartial", model);
         }
 
+        /// <summary>
+        /// Get method for fetching HTML for a TimeLineItem.
+        /// </summary>
+        /// <param name="model">TimeLineItemViewModel</param>
+        /// <returns>PartialView for the TimeLineItem type with the TimeLineItem as a model.</returns>
         [AllowAnonymous]
         public async Task<ActionResult> GetTimeLineItem(TimeLineItemViewModel model)
         {
@@ -220,6 +306,11 @@ namespace KinaUnaWeb.Controllers
             return PartialView(timeLineItemPartialViewModel.PartialViewName, timeLineItemPartialViewModel.TimeLineItem);
         }
 
+        /// <summary>
+        /// HttpPost method for fetching HTML for a TimeLineItem.
+        /// </summary>
+        /// <param name="model">TimeLineItemViewModel</param>
+        /// <returns>PartialView for the TimeLineItem type with the TimeLineItem as a model.</returns>
         [AllowAnonymous]
         [HttpPost]
         public async Task<ActionResult> GetTimelineItemElement([FromBody] TimeLineItemViewModel model)
