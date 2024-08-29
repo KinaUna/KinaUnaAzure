434beb5f269a1305a9ed7148b4848c84511d2b97
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-29 07:56:21 +0200

    Documentation update. Added summary comments for actions in Data/Models/ApplicationUser.cs

--- KinaUna.Data/Models/ApplicationUser.cs
+++ KinaUna.Data/Models/ApplicationUser.cs
@@ -8,12 +8,35 @@ namespace KinaUna.Data.Models
     /// </summary>
     public class ApplicationUser: IdentityUser
     {
+        /// <summary>
+        /// First name of the user.
+        /// </summary>
         public string FirstName { get; set; }
+        /// <summary>
+        /// Middle name of the user.
+        /// </summary>
         public string MiddleName { get; set; }
+        /// <summary>
+        /// Last name of the user.
+        /// </summary>
         public string LastName { get; set; }
+        /// <summary>
+        /// Default Progeny Id for the user.
+        /// Obsolete. Use UserInfo from ProgenyApi instead.
+        /// </summary>
         public int ViewChild { get; set; }
+        /// <summary>
+        /// User's time zone.
+        /// Only used during signup, then stored in UserInfo in ProgenyApi.
+        /// </summary>
         public string TimeZone { get; set; }
+        /// <summary>
+        /// The date the user signed up.
+        /// </summary>
         public DateTime JoinDate { get; set; }
+        /// <summary>
+        /// User role. Not used.
+        /// </summary>
         public string Role { get; set; }
     }
 }
