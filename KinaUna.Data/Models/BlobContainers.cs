8673aa16ce8b6c59a4c3200c47c5a2d6952114d9
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-08-29 08:00:59 +0200

    Documentation update. Added summary comments for actions in ProgenyApi/Models/BlobContainers.cs

--- KinaUna.Data/Models/BlobContainers.cs
+++ KinaUna.Data/Models/BlobContainers.cs
@@ -6,13 +6,37 @@
     /// </summary>
     public static class BlobContainers
     {
+        /// <summary>
+        /// Container for Progeny Pictures.
+        /// </summary>
         public const string Pictures = "pictures";
+        /// <summary>
+        /// Container for Progeny profile pictures.
+        /// </summary>
         public const string Progeny = "progeny";
+        /// <summary>
+        /// Container for User profile pictures.
+        /// </summary>
         public const string Profiles = "profiles";
+        /// <summary>
+        /// Container for Friends profile pictures.
+        /// </summary>
         public const string Friends = "friends";
+        /// <summary>
+        /// Container for Contacts profile pictures.
+        /// </summary>
         public const string Contacts = "contacts";
+        /// <summary>
+        /// Container for Notes attachments and embedded images.
+        /// </summary>
         public const string Notes = "notes";
+        /// <summary>
+        /// Container for documents.
+        /// </summary>
         public const string Documents = "documents";
+        /// <summary>
+        /// Container for attachments and embedded images in KinaUnaTexts.
+        /// </summary>
         public const string KinaUnaTexts = "kinaunatexts";
     }
 }
