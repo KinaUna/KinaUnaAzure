4bc7995f72d31a0dc29272cf5849b9c00b7ebb26
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-09-28 17:24:47 +0200

    Add CustomError and CustomResult<T> classes
    Introduce CustomError for error handling with predefined codes and logging.
    Add CustomResult<T> for operation results, supporting success and failure states.

---
+++ KinaUna.Data/Models/DTOs/CustomError.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace KinaUna.Data.Models.DTOs;
+
+public class CustomError
+{
+    private const string ValidationErrorCode = "ValidationError";
+    private const string HttpRequestErrorCode = "HttpRequestError";
+    private const string NotFoundErrorCode = "NotFoundError";
+    private const string ExceptionErrorCode = "ExceptionError";
+
+    private CustomError(string code, string message, ILogger? logger = null)
+    {
+        Code = code;
+        Message = message;
+        Logger = logger;
+    }
+
+    public string Code { get; init; }
+    public string Message { get; init; }
+    public ILogger? Logger { get; init; }
+
+
+    public static readonly CustomError None = new(string.Empty, string.Empty);
+    public static CustomError ValidationError(string message) => new(ValidationErrorCode, message);
+    public static CustomError HttpRequestError(string message, ILogger? logger = null) => new(HttpRequestErrorCode, message, logger);
+    public static CustomError NotFoundError(string message, ILogger? logger = null) => new(NotFoundErrorCode, message, logger);
+    public static CustomError ExceptionError(string message, ILogger? logger = null) => new(ExceptionErrorCode, message, logger);
+}
---
+++ KinaUna.Data/Models/DTOs/CustomResult.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace KinaUna.Data.Models.DTOs;
+
+public class CustomResult<T>
+{
+    private readonly T? _value;
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+
+    public static implicit operator CustomResult<T>(T value) => Success(value);
+    public static implicit operator CustomResult<T>(CustomError error) => Failure(error);
+
+    public T Value
+    {
+        get
+        {
+            if (IsFailure)
+            {
+                throw new InvalidOperationException("Value is not defined for failure.");
+            }
+
+            return _value!;
+        }
+
+        private init => _value = value;
+    }
+
+    public CustomError? Error { get; }
+
+    private CustomResult(T value)
+    {
+        Value = value;
+        IsSuccess = true;
+        Error = CustomError.None;
+    }
+
+    private CustomResult(CustomError error)
+    {
+        if (error == CustomError.None)
+        {
+            throw new InvalidOperationException("Error must be defined for failure.");
+        }
+
+        IsSuccess = false;
+        Error = error;
+    }
+
+    public static CustomResult<T> Success(T value) => new(value);
+
+    public static CustomResult<T> Failure(CustomError error)
+    {
+        error.Logger?.LogError("{ErrorCode}: {ErrorMessage}", error.Code, error.Message);
+        return new CustomResult<T>(error);
+    }
+
+    public static CustomResult<Exception> ExceptionCaughtFailure(Exception e, ILogger? logger = null)
+    {
+        return CustomResult<Exception>.Failure(CustomError.ExceptionError(e.Message, logger));
+    }
+}
