865a3bdc360af43af08d552a0d52b89703ea1933
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-10-03 11:53:52 +0200

    Add CalendarReminder entity and DbSet to ProgenyDbContext
    A new DbSet<CalendarReminder> property named CalendarRemindersDb
    has been added to the ProgenyDbContext class in the KinaUna.Data.
    Contexts namespace. This allows the context to manage CalendarReminder
    entities.
    
    A new class CalendarReminder has been created in the KinaUna.Data.
    Models namespace. This class includes the following properties:
    - CalendarReminderId (int): The unique identifier for the calendar reminder.
    - EventId (int): The identifier for the associated event.
    - UserId (string): The identifier for the user who set the reminder.
    - NotifyTime (DateTime): The time at which the user should be notified.
    - Notified (bool): A flag indicating whether the user has been notified.

---
+++ KinaUna.Data/Models/CalendarReminder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KinaUna.Data.Models
+{
+    /// <summary>
+    /// CalendarReminder entity. Used to store reminders for calendar events.
+    /// </summary>
+    public class CalendarReminder
+    {
+        public int CalendarReminderId { get; set; }
+        /// <summary>
+        /// The EventId of the event the reminder is for.
+        /// </summary>
+        public int EventId { get; set; }
+        /// <summary>
+        /// The UserId of the user the reminder is for.
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// The time to send a notification to remind the user.
+        /// </summary>
+        public DateTime NotifyTime { get; set; }
+        /// <summary>
+        /// Has the reminder been sent.
+        /// </summary>
+        public bool Notified { get; set; }
+    }
+}
