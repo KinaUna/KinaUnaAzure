2a6be39d9bd801ee36c0dd83aab67c37002b3295
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-10-14 18:44:39 +0200

    Fixed delete user access bug.
    Delete user access didn't work because the CurrentProgenyId property wasn't set for the form data passed to the DeleteAccess action in AccessManagementController.
    Added hidden input field to include the missing value.

--- KinaUnaWeb/wwwroot/scripts/addItem/add-item.ts
+++ KinaUnaWeb/wwwroot/scripts/addItem/add-item.ts
@@ -1,18 +1,18 @@
 import { hideBodyScrollbars, showBodyScrollbars } from "../item-details/items-display-v8.js";
-import { startLoadingItemsSpinner, stopLoadingItemsSpinner } from "../navigation-tools-v8.js";
+import { startFullPageSpinner, startLoadingItemsSpinner, stopFullPageSpinner, stopLoadingItemsSpinner } from "../navigation-tools-v8.js";
 
 export function setAddItemButtonEventListeners(): void {
     let addItemButtons = document.querySelectorAll<HTMLAnchorElement>('.add-item-button');
     addItemButtons.forEach(function (button) {
         button.addEventListener('click', async function (event) {
             event.preventDefault();
+            startFullPageSpinner();
             let addItemButton = event.currentTarget as HTMLAnchorElement;
-            console.log(addItemButton);
             let addItemType = addItemButton.getAttribute('data-add-item-type');
-            console.log(addItemType);
             if (addItemType !== null) {
                 await popupAddItemModal(addItemType as string);
             }
+            stopFullPageSpinner();
         });
     });
 }
@@ -20,6 +20,7 @@ export function setAddItemButtonEventListeners(): void {
 async function popupAddItemModal(addItemType: string): Promise<void> {
     let popup = document.getElementById('item-details-div');
     if (popup !== null) {
+        popup.innerHTML = '';
         await fetch('/AddItem/GetAddItemModalContent?itemType=' + addItemType, {
             method: 'GET',
             headers: {
@@ -28,7 +29,10 @@ async function popupAddItemModal(addItemType: string): Promise<void> {
         }).then(async function (response) {
             if (response.ok) {
                 let modalContent = await response.text();
-                popup.innerHTML = modalContent;
+                const fullScreenOverlay = document.createElement('div');
+                fullScreenOverlay.classList.add('full-screen-bg');
+                fullScreenOverlay.innerHTML = modalContent;
+                popup.appendChild(fullScreenOverlay);              
             }
         });
 
@@ -40,6 +44,7 @@ async function popupAddItemModal(addItemType: string): Promise<void> {
         ($(".selectpicker") as any).selectpicker('refresh');
         hideBodyScrollbars();
         addCloseButtonEventListener();
+        addCancelButtonEventListener();
         setSaveAddItemFormEventListener();
     }
 
@@ -65,12 +70,37 @@ function addCloseButtonEventListener(): void {
     }
 }
 
+/**
+ * Adds an event listener to the close button in the item details popup.
+ * When clicked, the popup is hidden and the body scrollbars are shown.
+ */
+function addCancelButtonEventListener(): void {
+    let closeButtonsList = document.querySelectorAll<HTMLButtonElement>('.item-details-cancel-button');
+    if (closeButtonsList) {
+        closeButtonsList.forEach((button) => {
+            button.addEventListener('click', function () {
+                const itemDetailsPopupDiv = document.querySelector<HTMLDivElement>('#item-details-div');
+                if (itemDetailsPopupDiv) {
+                    itemDetailsPopupDiv.innerHTML = '';
+                    itemDetailsPopupDiv.classList.add('d-none');
+                    showBodyScrollbars();
+                }
+            });
+        });
+    }
+}
+
 function setSaveAddItemFormEventListener(): void {
     let addItemForm = document.querySelector<HTMLFormElement>('#add-item-form');
     if (addItemForm) {
         addItemForm.addEventListener('submit', async function (event) {
             event.preventDefault();
-            startLoadingItemsSpinner('item-details-div')
+            startFullPageSpinner();
+            let itemDetailsPopupDiv = document.querySelector<HTMLDivElement>('#item-details-div');
+            if (itemDetailsPopupDiv) {
+                itemDetailsPopupDiv.classList.add('d-none');
+                itemDetailsPopupDiv.innerHTML = '';
+            }
             let formData = new FormData(addItemForm);
             let formAction = addItemForm.getAttribute('action');
             if (formAction) {
@@ -79,10 +109,12 @@ function setSaveAddItemFormEventListener(): void {
                     body: formData
                 }).then(async function (response) {
                     if (response.ok) {
-                        let itemDetailsPopupDiv = document.querySelector<HTMLDivElement>('#item-details-div');
                         if (itemDetailsPopupDiv) {
                             let modalContent = await response.text();
-                            itemDetailsPopupDiv.innerHTML = modalContent;
+                            const fullScreenOverlay = document.createElement('div');
+                            fullScreenOverlay.classList.add('full-screen-bg');
+                            fullScreenOverlay.innerHTML = modalContent;
+                            itemDetailsPopupDiv.appendChild(fullScreenOverlay);        
                             itemDetailsPopupDiv.classList.remove('d-none');
                             hideBodyScrollbars();
                             addCloseButtonEventListener();
@@ -90,8 +122,7 @@ function setSaveAddItemFormEventListener(): void {
                     }
                 });
             }
-
-            stopLoadingItemsSpinner('item-details-div');
+            stopFullPageSpinner();
         });
     }
 }
