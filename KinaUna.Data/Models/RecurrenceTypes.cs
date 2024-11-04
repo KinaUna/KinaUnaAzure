0052d0d5898a475c4b6761ff3a462cfb8c856966
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2024-11-04 09:11:52 +0100

    Add RecurrenceTypes class with RecurrenceFrequency enum
    A new namespace `KinaUna.Data.Models` has been added.
    Within this namespace, a new static class `RecurrenceTypes` has been introduced.
    Inside the `RecurrenceTypes` class, an enumeration `RecurrenceFrequency` has been defined.
    The `RecurrenceFrequency` enum includes the following values to represent different types of recurrence rules:
    - `Daily` with a value of 0
    - `Weekly` with a value of 1
    - `MonthlyByDay` with a value of 2
    - `MonthlyByDate` with a value of 3
    - `YearlyByDay` with a value of 4
    - `YearlyByDate` with a value of 5
    
    This provides a structured way to handle recurrence rule types.

---
+++ KinaUna.Data/Models/RecurrenceTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace KinaUna.Data.Models
+{
+    /// <summary>
+    /// Types of recurrence rules.
+    /// </summary>
+    public static class RecurrenceTypes
+    {
+        /// <summary>
+        /// The frequency of the recurrence rule.
+        /// daily = 0, weekly = 1, monthly by day = 2, monthly by date = 3, yearly by day = 4, yearly by date = 5
+        /// </summary>
+        public enum RecurrenceFrequency { Daily = 0, Weekly = 1, MonthlyByDay = 2, MonthlyByDate = 3, YearlyByDay = 4, YearlyByDate = 5  }
+    }
+}
