f061ec6a16d16ac938094317babbfcc058d0f5aa
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-06-19 17:47:27 +0200

    Clean up client configurations in Config.cs
    Removed commented-out client configurations for `KinaUnaWebBlazor`, `KinaUnaXamarin`, and others. Added active client configurations for `KinaUnaWeb` and `KinaUnaWebLocal`, specifying essential details like `ClientName`, `ClientId`, `ClientUri`, and `ClientSecrets`. Updated `AllowedCorsOrigins` and `RedirectUris` to enhance security and functionality.

--- KinaUna.IDP/Config.cs
+++ KinaUna.IDP/Config.cs
@@ -99,10 +99,8 @@ namespace KinaUna.IDP
         public static List<Client> GetClients(IConfiguration configuration)
         {
             string webServerUrl = configuration.GetValue<string>("WebServer");
-            // string webBlazorServerUrl = configuration.GetValue<string>("WebBlazorServer");
             string webServerAzureUrl = configuration.GetValue<string>("WebServerAzure");
             string webServerLocal = configuration.GetValue<string>("WebServerLocal");
-            // string webBlazorServerLocal = configuration.GetValue<string>("WebBlazorServerLocal");
             string secretString = configuration.GetValue<string>("SecretString");
             List<string> corsList =
             [
@@ -114,43 +112,6 @@ namespace KinaUna.IDP
             ];
             return
             [
-                //new Client
-                //{
-                //    ClientName = "KinaUnaWebBlazor",
-                //    ClientId = "kinaunawebblazorclient",
-                //    ClientUri = webBlazorServerUrl,
-                //    RequirePkce = false,
-                //    AllowPlainTextPkce = false,
-                //    RequireConsent = false,
-                //    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
-                //    AccessTokenType = AccessTokenType.Reference,
-                //    AccessTokenLifetime = 2592000,
-                //    AllowOfflineAccess = true,
-                //    AlwaysIncludeUserClaimsInIdToken = false,
-                //    UpdateAccessTokenClaimsOnRefresh = true,
-                //    RedirectUris =
-                //    [
-                //        webServerUrl + "/signin-oidc"
-
-                //    ],
-                //    PostLogoutRedirectUris =
-                //    [
-                //        webServerUrl + "/signout-callback-oidc"
-                //    ],
-                //    AllowedScopes =
-                //    {
-                //        IdentityServerConstants.StandardScopes.OpenId,
-                //        IdentityServerConstants.StandardScopes.Profile,
-                //        IdentityServerConstants.StandardScopes.OfflineAccess,
-                //        IdentityServerConstants.StandardScopes.Email,
-                //        Constants.ProgenyApiName,
-                //        Constants.MediaApiName
-                //    },
-                //    ClientSecrets =
-                //    {
-                //        new Secret(secretString.Sha256())
-                //    }
-                //},
                 new Client
                 {
                     ClientName = "KinaUnaWeb",
@@ -196,43 +157,6 @@ namespace KinaUna.IDP
                         new Secret(secretString.Sha256())
                     }
                 },
-                //new Client
-                //{
-                //    ClientName = "KinaUnaWebBlazorLocal",
-                //    ClientId = "kinaunawebblazorclientlocal",
-                //    ClientUri = webBlazorServerLocal,
-                //    RequirePkce = false,
-                //    AllowPlainTextPkce = false,
-                //    RequireConsent = false,
-                //    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
-                //    AccessTokenType = AccessTokenType.Reference,
-                //    AccessTokenLifetime = 2592000,
-                //    AllowOfflineAccess = true,
-                //    AlwaysIncludeUserClaimsInIdToken = false,
-                //    UpdateAccessTokenClaimsOnRefresh = true,
-                //    RedirectUris =
-                //    [
-                //        webBlazorServerLocal + "/signin-oidc"
-
-                //    ],
-                //    PostLogoutRedirectUris =
-                //    [
-                //        webBlazorServerLocal + "/signout-callback-oidc"
-                //    ],
-                //    AllowedScopes =
-                //    {
-                //        IdentityServerConstants.StandardScopes.OpenId,
-                //        IdentityServerConstants.StandardScopes.Profile,
-                //        IdentityServerConstants.StandardScopes.OfflineAccess,
-                //        IdentityServerConstants.StandardScopes.Email,
-                //        Constants.ProgenyApiName,
-                //        Constants.MediaApiName
-                //    },
-                //    ClientSecrets =
-                //    {
-                //        new Secret(secretString.Sha256())
-                //    }
-                //},
                 new Client
                 {
                     ClientName = "KinaUnaWebLocal",
@@ -290,7 +214,6 @@ namespace KinaUna.IDP
                     AllowOfflineAccess = true,
                     AlwaysIncludeUserClaimsInIdToken = false,
                     RefreshTokenExpiration = TokenExpiration.Sliding,
-                    //AbsoluteRefreshTokenLifetime = ...
                     UpdateAccessTokenClaimsOnRefresh = true,
                     AllowedCorsOrigins = corsList,
                     RedirectUris =
@@ -319,128 +242,7 @@ namespace KinaUna.IDP
                     {
                         new Secret(secretString.Sha256())
                     }
-                },
-                //new Client
-                //{
-                //    ClientName = "KinaUnaXamarin",
-                //    ClientId = "kinaunaxamarin",
-                //    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
-                //    RedirectUris = {"kinaunaxamarinclients://callback"},
-                //    PostLogoutRedirectUris = {"kinaunaxamarinclients://callback"},
-                //    RequireClientSecret = false,
-                //    RequireConsent = false,
-                //    RequirePkce = true,
-                //    AllowedScopes = {
-                //        IdentityServerConstants.StandardScopes.OpenId,
-                //        IdentityServerConstants.StandardScopes.Profile,
-                //        IdentityServerConstants.StandardScopes.Email,
-                //        "firstname",
-                //        "middlename",
-                //        "lastname",
-                //        "roles",
-                //        "timezone",
-                //        "viewchild",
-                //        "joindate",
-                //        Constants.ProgenyApiName,
-                //        Constants.MediaApiName
-                //    },
-                //    AccessTokenLifetime = 2592000,
-                //    AllowOfflineAccess = true,
-                //    AlwaysIncludeUserClaimsInIdToken = false,
-                //    RefreshTokenExpiration = TokenExpiration.Sliding,
-                //    UpdateAccessTokenClaimsOnRefresh = true,
-                //    RefreshTokenUsage = TokenUsage.ReUse
-                    
-                //},
-                //new Client
-                //{
-                //    ClientName = "KinaUnaXamarin2",
-                //    ClientId = "kinaunaxamarin2",
-                //    AllowedGrantTypes = GrantTypes.Code,
-                //    RedirectUris = {"kinaunaxamarinclients://callback"},
-                //    PostLogoutRedirectUris = {"kinaunaxamarinclients://callback"},
-                //    RequireClientSecret = false,
-                //    RequireConsent = false,
-                //    RequirePkce = true,
-                //    AllowedScopes = {
-                //        IdentityServerConstants.StandardScopes.OpenId,
-                //        IdentityServerConstants.StandardScopes.Profile,
-                //        IdentityServerConstants.StandardScopes.Email,
-                //        "firstname",
-                //        "middlename",
-                //        "lastname",
-                //        "roles",
-                //        "timezone",
-                //        "viewchild",
-                //        "joindate",
-                //        Constants.ProgenyApiName,
-                //        Constants.MediaApiName
-                //    },
-                //    AccessTokenLifetime = 2592000,
-                //    AllowOfflineAccess = true,
-                //    AlwaysIncludeUserClaimsInIdToken = false,
-                //    RefreshTokenExpiration = TokenExpiration.Sliding,
-                //    UpdateAccessTokenClaimsOnRefresh = true,
-                //    RefreshTokenUsage = TokenUsage.ReUse
-
-                //},
-                //new Client
-                //{
-                //    ClientName = "KinaUnaXamarin3",
-                //    ClientId = "kinaunaxamarin3",
-                //    AllowedGrantTypes = GrantTypes.Code,
-                //    RedirectUris = { "net.kinauna.xamarin://callback" },
-                //    PostLogoutRedirectUris = { "net.kinauna.xamarin://callback" },
-                //    RequireClientSecret = false,
-                //    RequireConsent = false,
-                //    RequirePkce = true,
-                //    AllowedScopes =
-                //    {
-                //        IdentityServerConstants.StandardScopes.OpenId,
-                //        IdentityServerConstants.StandardScopes.Profile,
-                //        IdentityServerConstants.StandardScopes.Email,
-                //        "firstname",
-                //        "middlename",
-                //        "lastname",
-                //        "roles",
-                //        "timezone",
-                //        "viewchild",
-                //        "joindate",
-                //        Constants.ProgenyApiName,
-                //        Constants.MediaApiName
-                //    },
-                //    AccessTokenLifetime = 2592000,
-                //    AllowOfflineAccess = true,
-                //    AlwaysIncludeUserClaimsInIdToken = false,
-                //    RefreshTokenExpiration = TokenExpiration.Sliding,
-                //    UpdateAccessTokenClaimsOnRefresh = true,
-                //    RefreshTokenUsage = TokenUsage.ReUse
-
-                //},
-                //new Client
-                //{
-                //    ClientName = "KinaUnaMaui",
-                //    ClientId = "kinaunamaui",
-                //    AllowedGrantTypes = GrantTypes.Code,
-                //    RedirectUris = { "kinaunamaui://callback" },
-                //    PostLogoutRedirectUris = { "kinaunamaui://callback" },
-                //    RequireClientSecret = false,
-                //    RequireConsent = false,
-                //    RequirePkce = true,
-                //    AllowedScopes =
-                //    {
-                //        IdentityServerConstants.StandardScopes.OpenId,
-                //        IdentityServerConstants.StandardScopes.Email,
-                //        Constants.ProgenyApiName,
-                //        Constants.MediaApiName
-                //    },
-                //    AccessTokenLifetime = 2592000,
-                //    AllowOfflineAccess = true,
-                //    AlwaysIncludeUserClaimsInIdToken = false,
-                //    RefreshTokenExpiration = TokenExpiration.Sliding,
-                //    UpdateAccessTokenClaimsOnRefresh = true,
-                //    RefreshTokenUsage = TokenUsage.ReUse
-                //}
+                }
             ];
 
         }
