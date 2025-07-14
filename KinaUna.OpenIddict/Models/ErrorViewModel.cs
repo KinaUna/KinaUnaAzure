552a06a50f21a2db11bed0bdaeac8bd2f8f8f4b8
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-07-14 10:56:03 +0200

    Created new project for the OpenIddict server using the ASP.NET Web application with MVC template.

---
+++ KinaUna.OpenIddict/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KinaUna.OpenIddict.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
