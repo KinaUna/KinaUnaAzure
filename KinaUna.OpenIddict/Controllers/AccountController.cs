3f051fe53518a735bd770f126169149cac699162
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-07-14 15:02:09 +0200

    Add authentication and data protection features
    Updated project files to include new package references for Azure Blob Storage and Entity Framework Core. Enhanced `Program.cs` with dependency injection for database contexts and added localization support. Created `AccountController` for user login/logout functionality, introduced `LoginViewModel`, and added a new Razor view for the login form.

---
+++ KinaUna.OpenIddict/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using KinaUna.OpenIddict.Models.AccountViewModels;
+
+namespace KinaUna.OpenIddict.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            ViewData["ReturnUrl"] = model.ReturnUrl;
+
+            if (ModelState.IsValid)
+            {
+                List<Claim> claims = [new Claim(ClaimTypes.Name, model.Username)];
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+
+                if (Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ KinaUna.OpenIddict/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KinaUna.OpenIddict.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required] public required string Username { get; set; }
+        [Required] public required string Password { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
