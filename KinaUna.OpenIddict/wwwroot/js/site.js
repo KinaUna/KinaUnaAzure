ce9679974482033cb97b1f4e86846ed9d2b521be
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-07-14 12:30:27 +0200

    Copied basic files from KinaUna.IDP to KinaUna.OpenIddict to keep the same UI functionality, as well as look and feel.

--- KinaUna.OpenIddict/wwwroot/js/site.js
+++ KinaUna.OpenIddict/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.domain = document.domain;
+$(document).ready(function () {
+    $(".closeModal").click(function () {
+        window.parent.postMessage("closeModal", '*');
+    });
 
-// Write your JavaScript code.
+    $(".leavePage").click(function () {
+        runWaitMeLeave();
+        function runWaitMeLeave() {
+            $('.body-content').waitMe({
+                //none, rotateplane, stretch, orbit, roundBounce, win8,
+                //win8_linear, ios, facebook, rotation, timer, pulse,
+                //progressBar, bouncePulse or img
+                effect: 'roundBounce',
+                //place text under the effect (string).
+                text: '',
+                //background for container (string).
+                bg: 'rgba(225,240,215,0.3)',
+                //color for background animation and text (string).
+                color: ['#6a5081', '#7a6095', '#8a70aa', '#9a80bb', '#aa90cc', '#bbaadd', '#ccbbee', '#ddccff', '#ccbbee', '#bbaadd', '#aa90cc', '#9a80bb', '#6a5081', '#7a6095', '#8a70aa', '#9a80bb', '#aa90cc', '#bbaadd', '#ccbbee', '#ddccff', '#ccbbee', '#bbaadd', '#aa90cc', '#9a80bb'],
+                //max size
+                maxSize: '',
+                //wait time im ms to close
+                waitTime: -1,
+                //url to image
+                source: '',
+                //or 'horizontal'
+                textPos: 'vertical',
+                //font size
+                fontSize: '',
+                // callback
+                onClose: function () { }
+            });
+        }
+    });
+});
