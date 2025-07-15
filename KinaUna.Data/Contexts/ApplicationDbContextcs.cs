8f3f89f53436ca6f21fa8f33649ebb68b1ec7f97
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-07-15 14:17:21 +0200

    Removed UseOpenIddict from ApplicationDbContext as it is added in the database configuration.

--- KinaUna.Data/Contexts/ApplicationDbContextcs.cs
+++ KinaUna.Data/Contexts/ApplicationDbContextcs.cs
@@ -6,11 +6,5 @@ namespace KinaUna.Data.Contexts
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            builder.UseOpenIddict(); // Add this line
-        }
     }
 }
