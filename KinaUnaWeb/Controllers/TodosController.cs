7a796d16f46963f3e109c07788e80b3ef10644c4
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-08-05 14:06:41 +0200

    Add TodosController and TodoItemsListViewModel
    Introduce a new `TodosController` for managing todo items, including methods for displaying and retrieving todo items with timezone adjustments and access level filtering. Create `TodoItemsListViewModel` to hold a list of todo items and manage popup item IDs, along with necessary date conversions. Update using directives for required namespaces.

---
+++ KinaUnaWeb/Controllers/TodosController.cs
@@ -0,0 +1,85 @@
+﻿using KinaUna.Data.Extensions;
+using KinaUna.Data.Models.DTOs;
+using KinaUnaWeb.Models;
+using KinaUnaWeb.Models.ItemViewModels;
+using KinaUnaWeb.Services;
+using KinaUnaWeb.Services.HttpClients;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KinaUnaWeb.Controllers
+{
+    public class TodosController(IViewModelSetupService viewModelSetupService, IUserInfosHttpClient userInfosHttpClient, IProgenyHttpClient progenyHttpClient) : Controller
+    {
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(int? todoItemId, int childId = 0)
+        {
+            BaseItemsViewModel baseModel = await viewModelSetupService.SetupViewModel(Request.GetLanguageIdFromCookie(), User.GetEmail(), childId);
+            TodoItemsListViewModel model = new(baseModel)
+            {
+                PopUpTodoItemId = todoItemId ?? 0
+            };
+
+            if (model.PopUpTodoItemId != 0)
+            {
+                model.TodoItemsList.Add(await todoItemsHttpClient.GetCalendarItem(model.PopUpTodoItemId));
+            }
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> GetTodoItemsList([FromBody] TodoItemsRequest request)
+        {
+            UserInfo currentUserInfo = await userInfosHttpClient.GetUserInfo(User.GetEmail());
+            request.StartDate = new DateTime(request.StartYear, request.StartMonth, request.StartDay);
+            request.EndDate = new DateTime(request.EndYear, request.EndMonth, request.EndDay);
+
+            List<TodoItem> todoItems = await todoItemsHttpClient.GetProgeniesTodoItemsList(request);
+
+            todoItems = [.. todoItems.OrderBy(t => t.DueDate)];
+            List<TodoItem> resultList = [];
+            List<Progeny> progeniesList = [];
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                if (todoItem.DueDate.HasValue)
+                {
+                    todoItem.DueDate = TimeZoneInfo.ConvertTimeFromUtc(todoItem.DueDate.Value,
+                        TimeZoneInfo.FindSystemTimeZoneById(currentUserInfo.Timezone));
+                }
+
+                if (todoItem.StartDate.HasValue)
+                {
+                    todoItem.StartDate = TimeZoneInfo.ConvertTimeFromUtc(todoItem.StartDate.Value,
+                        TimeZoneInfo.FindSystemTimeZoneById(currentUserInfo.Timezone));
+                }
+
+                if (todoItem.CompletedDate.HasValue)
+                {
+                    todoItem.CompletedDate = TimeZoneInfo.ConvertTimeFromUtc(todoItem.CompletedDate.Value,
+                        TimeZoneInfo.FindSystemTimeZoneById(currentUserInfo.Timezone));
+                }
+
+                todoItem.CreatedTime = TimeZoneInfo.ConvertTimeFromUtc(todoItem.CreatedTime,
+                    TimeZoneInfo.FindSystemTimeZoneById(currentUserInfo.Timezone));
+
+                Progeny progeny = progeniesList.FirstOrDefault(p => p.Id == todoItem.ProgenyId);
+                if (progeny == null)
+                {
+                    progeny = await progenyHttpClient.GetProgeny(todoItem.ProgenyId);
+                    progeniesList.Add(progeny);
+                }
+                
+                resultList.Add(todoItem);
+            }
+
+            return Json(resultList);
+        }
+    }
+}
---
+++ KinaUnaWeb/Models/ItemViewModels/TodoItemsListViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KinaUnaWeb.Models.ItemViewModels
+{
+    public class TodoItemsListViewModel: BaseItemsViewModel
+    {
+        public List<TodoItem> TodoItemsList { get; set; }
+        
+        
+        public int PopUpTodoItemId = 0;
+
+        public TodoItemsListViewModel(BaseItemsViewModel baseItemsViewModel)
+        {
+            SetBaseProperties(baseItemsViewModel);
+
+            TodoItemsList = [];
+        }
+        
+        public void SetTodoItemsList(List<TodoItem> todoItemsList)
+        {
+            todoItemsList = [.. todoItemsList.OrderBy(t => t.DueDate).ThenBy(t => t.CreatedTime)];
+            TodoItemsList = [];
+
+            foreach (TodoItem todoItem in todoItemsList)
+            {
+                if (todoItem.AccessLevel != (int)AccessLevel.Public && todoItem.AccessLevel < CurrentAccessLevel) continue;
+
+                if (todoItem.DueDate.HasValue)
+                {
+                    todoItem.DueDate = TimeZoneInfo.ConvertTimeFromUtc(todoItem.DueDate.Value,
+                        TimeZoneInfo.FindSystemTimeZoneById(CurrentUser.Timezone));
+                }
+
+                if (todoItem.StartDate.HasValue)
+                {
+                    todoItem.StartDate = TimeZoneInfo.ConvertTimeFromUtc(todoItem.StartDate.Value,
+                        TimeZoneInfo.FindSystemTimeZoneById(CurrentUser.Timezone));
+                }
+
+                if (todoItem.CompletedDate.HasValue)
+                {
+                    todoItem.CompletedDate = TimeZoneInfo.ConvertTimeFromUtc(todoItem.CompletedDate.Value,
+                        TimeZoneInfo.FindSystemTimeZoneById(CurrentUser.Timezone));
+                }
+
+                todoItem.CreatedTime = TimeZoneInfo.ConvertTimeFromUtc(todoItem.CreatedTime,
+                    TimeZoneInfo.FindSystemTimeZoneById(CurrentUser.Timezone));
+
+                TodoItemsList.Add(todoItem);
+            }
+        }
+    }
+}
