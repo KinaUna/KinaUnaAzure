68182ca3ea0f0a63f5431340290a0fa15d8d1e2d
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-08-26 19:16:12 +0200

    Add KanbanBoards and KanbanItems controllers
    Implemented two new API controllers: KanbanBoardsController and KanbanItemsController. Both controllers enforce user authorization with the "UserOrClient" policy, produce JSON responses, and are set up with appropriate routing and API behaviors.

---
+++ KinaUnaProgenyApi/Controllers/KanbanBoardsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KinaUnaProgenyApi.Controllers
+{
+    [Authorize(Policy = "UserOrClient")]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KanbanBoardsController : ControllerBase
+    {
+    }
+}
---
+++ KinaUnaProgenyApi/Controllers/KanbanItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KinaUnaProgenyApi.Controllers
+{
+    [Authorize(Policy = "UserOrClient")]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KanbanItemsController : Controller
+    {
+        
+    }
+}
