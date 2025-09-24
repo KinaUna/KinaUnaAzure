411a24c0609b0e21e2293879f3faa7310f3f0b0a
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-09-24 19:14:08 +0200

    Added EF migration for Family entities and Access Management entities.

---
+++ KinaUna.OpenIddict/Migrations/ProgenyDb/20250924171245_AddFamilyDbAndAccessManagementDb.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KinaUna.OpenIddict.Migrations.ProgenyDb
+{
+    /// <inheritdoc />
+    public partial class AddFamilyDbAndAccessManagementDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FamiliesDb",
+                columns: table => new
+                {
+                    FamilyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", maxLength: 4096, nullable: true),
+                    Admins = table.Column<string>(type: "nvarchar(max)", maxLength: 4096, nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FamiliesDb", x => x.FamilyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FamilyMembersDb",
+                columns: table => new
+                {
+                    FamilyMemberId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FamilyId = table.Column<int>(type: "int", nullable: false),
+                    MemberType = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ProgenyId = table.Column<int>(type: "int", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FamilyMembersDb", x => x.FamilyMemberId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FamilyPermissionsDb",
+                columns: table => new
+                {
+                    FamilyPermissionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FamilyId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    GroupId = table.Column<int>(type: "int", nullable: false),
+                    PermissionLevel = table.Column<int>(type: "int", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FamilyPermissionsDb", x => x.FamilyPermissionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProgenyPermissionsDb",
+                columns: table => new
+                {
+                    ProgenyPermissionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProgenyId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    GroupId = table.Column<int>(type: "int", nullable: false),
+                    PermissionLevel = table.Column<int>(type: "int", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgenyPermissionsDb", x => x.ProgenyPermissionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimelineItemPermissionsDb",
+                columns: table => new
+                {
+                    TimelineItemPermissionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TimelineType = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    ProgenyId = table.Column<int>(type: "int", nullable: false),
+                    FamilyId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    GroupId = table.Column<int>(type: "int", nullable: false),
+                    PermissionLevel = table.Column<int>(type: "int", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimelineItemPermissionsDb", x => x.TimelineItemPermissionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroupMembersDb",
+                columns: table => new
+                {
+                    UserGroupMemberId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    UserGroupId = table.Column<int>(type: "int", nullable: false),
+                    UserOwnerUserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    FamilyOwnerId = table.Column<int>(type: "int", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroupMembersDb", x => x.UserGroupMemberId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroupsDb",
+                columns: table => new
+                {
+                    UserGroupId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsFamily = table.Column<bool>(type: "bit", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", maxLength: 4096, nullable: true),
+                    ProgenyId = table.Column<int>(type: "int", nullable: false),
+                    FamilyId = table.Column<int>(type: "int", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroupsDb", x => x.UserGroupId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FamiliesDb");
+
+            migrationBuilder.DropTable(
+                name: "FamilyMembersDb");
+
+            migrationBuilder.DropTable(
+                name: "FamilyPermissionsDb");
+
+            migrationBuilder.DropTable(
+                name: "ProgenyPermissionsDb");
+
+            migrationBuilder.DropTable(
+                name: "TimelineItemPermissionsDb");
+
+            migrationBuilder.DropTable(
+                name: "UserGroupMembersDb");
+
+            migrationBuilder.DropTable(
+                name: "UserGroupsDb");
+        }
+    }
+}
