78f5b22abdce1f34a29cebd37fa6ed75c58e73fd
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-09-28 14:08:53 +0200

    Add new properties to Progeny class
    Updated the `Progeny` class in the `KinaUna.Data.Models` namespace to include new properties: `NickName`, `BirthDay`, `TimeZone`, `PictureLink`, `Admins`, `Email`, and `UserId`. Each property has been annotated for validation and includes XML documentation comments to clarify their purpose.

--- KinaUna.Data/Models/Progeny.cs
+++ KinaUna.Data/Models/Progeny.cs
@@ -8,26 +8,57 @@ namespace KinaUna.Data.Models
     /// </summary>
     public class Progeny
     {
+        /// <summary>
+        /// Auto-incremented primary key for the Progeny entity.
+        /// </summary>
         [Key]
         public int Id { get; set; }
 
+        /// <summary>
+        /// The name of the progeny.
+        /// </summary>
         [MaxLength(256)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// The nickname of the progeny. This is used when displaying the name of the progeny in the UI.
+        /// </summary>
         [MaxLength(256)]
         public string NickName { get; set; }
+
+        /// <summary>
+        /// The date and time of birth of the progeny.
+        /// </summary>
         public DateTime? BirthDay { get; set; }
 
+        /// <summary>
+        /// The timezone of the Birthday. This is used to calculate the age of the progeny correctly.
+        /// </summary>
         [MaxLength(256)]
         public string TimeZone { get; set; }
 
+        /// <summary>
+        /// The filename of the picture of the progeny. This is used to display the picture of the progeny in the UI.
+        /// </summary>
         [MaxLength(512)]
         public string PictureLink { get; set; }
 
+        /// <summary>
+        /// Comma separated list of emails of the administrators for the progeny.
+        /// </summary>
         [MaxLength(1024)]
         public string Admins { get; set; } // Comma separated list of emails.
 
+        /// <summary>
+        /// The email associated with the KinaUna account for the progeny.
+        /// </summary>
+        [MaxLength(1024)]
         public string Email { get; set; } // KinaUna account email
+        
+        /// <summary>
+        /// Gets or sets the unique identifier for the user associated with the KinaUna account.
+        /// </summary>
+        [MaxLength(256)]
         public string UserId { get; set; } // KinaUna account UserId
 
         /// <summary>
