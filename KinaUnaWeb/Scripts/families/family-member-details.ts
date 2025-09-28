3c00392c5f8f78069d39cbf55ef0b97e2b909fc6
Per Rosing von Philipsborn <per.von.philipsborn@kinauna.com>
2025-09-28 11:02:21 +0200

    Update family member details modal functionality
    Refactor `family-member-details.ts` and `family-member-details.js` to import new functions for item button event handling and scrollbar management. Change modal selector from `#modal-div` to `#item-details-div`. Implement `setFamilyMemberDetailsEventListeners` for managing close button actions and ensure proper event listeners are set for editing and deleting items.

--- KinaUnaWeb/Scripts/families/family-member-details.ts
+++ KinaUnaWeb/Scripts/families/family-member-details.ts
@@ -1,4 +1,7 @@
-﻿export async function displayFamilyMemberDetails(familyMemberId: number): Promise<void> {
+﻿import { setDeleteItemButtonEventListeners, setEditItemButtonEventListeners } from "../addItem/add-item.js";
+import { hideBodyScrollbars, showBodyScrollbars } from "../item-details/items-display-v9.js";
+
+export async function displayFamilyMemberDetails(familyMemberId: number): Promise<void> {
     const response = await fetch('/Families/FamilyMemberDetails?familyMemberId=' + familyMemberId, {
         method: 'GET',
         headers: {
@@ -7,11 +10,16 @@
     });
     if (response.ok) {
         const familyMemberDetails = await response.text();
-        const modalDiv = document.querySelector<HTMLDivElement>('#modal-div');
+        const modalDiv = document.querySelector<HTMLDivElement>('#item-details-div');
         if (modalDiv) {
             modalDiv.innerHTML = familyMemberDetails;
+            hideBodyScrollbars();
             modalDiv.classList.remove('d-none');
-            // Todo: Add event listeners
+
+            setFamilyMemberDetailsEventListeners(familyMemberId);
+            setEditItemButtonEventListeners();
+            setDeleteItemButtonEventListeners();
+
             return Promise.resolve();
         } else {
             return Promise.reject('Modal div not found in the document.');
@@ -22,3 +30,21 @@
     }
 }
 
+function setFamilyMemberDetailsEventListeners(familyMemberId: number): void {
+    let closeButtonsList = document.querySelectorAll('.item-details-close-button');
+    if (closeButtonsList) {
+        closeButtonsList.forEach((button) => {
+            const closeButtonActions = function () {
+                const modalDiv = document.querySelector<HTMLDivElement>('#item-details-div');
+                if (!modalDiv) return;
+
+                modalDiv.innerHTML = '';
+                modalDiv.classList.add('d-none');
+                showBodyScrollbars();
+            };
+            // Clear existing event listeners.
+            button.removeEventListener('click', closeButtonActions);
+            button.addEventListener('click', closeButtonActions);
+        });
+    }
+}
--- KinaUnaWeb/wwwroot/js/families/family-member-details.js
+++ KinaUnaWeb/wwwroot/js/families/family-member-details.js
@@ -1,3 +1,5 @@
+import { setDeleteItemButtonEventListeners, setEditItemButtonEventListeners } from "../addItem/add-item.js";
+import { hideBodyScrollbars, showBodyScrollbars } from "../item-details/items-display-v9.js";
 export async function displayFamilyMemberDetails(familyMemberId) {
     const response = await fetch('/Families/FamilyMemberDetails?familyMemberId=' + familyMemberId, {
         method: 'GET',
@@ -7,11 +9,14 @@ export async function displayFamilyMemberDetails(familyMemberId) {
     });
     if (response.ok) {
         const familyMemberDetails = await response.text();
-        const modalDiv = document.querySelector('#modal-div');
+        const modalDiv = document.querySelector('#item-details-div');
         if (modalDiv) {
             modalDiv.innerHTML = familyMemberDetails;
+            hideBodyScrollbars();
             modalDiv.classList.remove('d-none');
-            // Todo: Add event listeners
+            setFamilyMemberDetailsEventListeners(familyMemberId);
+            setEditItemButtonEventListeners();
+            setDeleteItemButtonEventListeners();
             return Promise.resolve();
         }
         else {
@@ -21,6 +26,24 @@ export async function displayFamilyMemberDetails(familyMemberId) {
     else {
         console.error('Failed to fetch family member details:', response.statusText);
         return Promise.reject('Failed to fetch family member details: ' + response.statusText);
+    }
+}
+function setFamilyMemberDetailsEventListeners(familyMemberId) {
+    let closeButtonsList = document.querySelectorAll('.item-details-close-button');
+    if (closeButtonsList) {
+        closeButtonsList.forEach((button) => {
+            const closeButtonActions = function () {
+                const modalDiv = document.querySelector('#item-details-div');
+                if (!modalDiv)
+                    return;
+                modalDiv.innerHTML = '';
+                modalDiv.classList.add('d-none');
+                showBodyScrollbars();
+            };
+            // Clear existing event listeners.
+            button.removeEventListener('click', closeButtonActions);
+            button.addEventListener('click', closeButtonActions);
+        });
     }
 }
 //# sourceMappingURL=family-member-details.js.map
